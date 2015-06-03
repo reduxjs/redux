7a456ec17b896aa11498539b1b0ee38ea4dfff17
Dan Abramov <dan.abramov@gmail.com>
2015-06-03 13:41:22 +0300

    Use @root decorator in the examples

--- examples/counter/App.js
+++ examples/counter/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import { Root, Container } from 'redux';
+import { root, Container } from 'redux';
 import { increment, decrement } from './actions/CounterActions';
 import counterStore from './stores/counterStore';
 import Counter from './Counter';
 
+@root
 export default class CounterApp extends Component {
   render() {
     return (
-      <Root>
-        {() =>
-          <Container stores={counterStore} actions={{ increment, decrement }}>
-            {props => <Counter {...props} />}
-          </Container>
-        }
-      </Root>
+      <Container stores={counterStore} actions={{ increment, decrement }}>
+        {props => <Counter {...props} />}
+      </Container>
     );
   }
 }
--- examples/todo/App.js
+++ examples/todo/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import Header from './Header';
 import Body from './Body';
-import { Root, Container } from 'redux';
+import { root, Container } from 'redux';
 import { todoStore } from './stores/index';
 import { addTodo } from './actions/index';
 
+@root
 export default class TodoApp {
   render() {
     return (
-      <Root>
-        {() =>
-          <Container stores={todoStore} actions={{ addTodo }}>
-            {props =>
-              <div>
-                <Header addTodo={props.addTodo} />
-                <Body todos={props.todos} />
-              </div>
-            }
-          </Container>
+      <Container stores={todoStore} actions={{ addTodo }}>
+        {props =>
+          <div>
+            <Header addTodo={props.addTodo} />
+            <Body todos={props.todos} />
+          </div>
         }
-      </Root>
+      </Container>
     );
   }
 }
