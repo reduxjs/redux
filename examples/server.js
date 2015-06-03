57ad124b6e9587a7c929513a8c2ddae487bfc78d
Mike Kidder <mikekidder@gmail.com>
2015-06-02 21:48:33 -0500

    a little splash of color for webpack :)

--- examples/server.js
+++ examples/server.js
@@ -5,8 +5,11 @@ var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+  historyApiFallback: true,
+  stats: {
+    colors: true
+  }
+}).listen(3000, 'localhost', function (err) {
   if (err) {
     console.log(err);
   }
