cb92d7815764e87419abb1301edd56f0a0fb768c
Vyacheslav Slinko <vslinko@yahoo.com>
2015-06-04 18:51:00 +0300

    Pass custom dispatcher to root decorator

--- src/addons/root.js
+++ src/addons/root.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Root from '../Root';
 import getDisplayName from './getDisplayName';
 
-export default function root(stores) {
+export default function root(stores, dispatcher = null) {
   return DecoratedComponent => class ReduxRootDecorator {
     static displayName = `ReduxRoot(${getDisplayName(DecoratedComponent)})`;
 
     render() {
       return (
-        <Root stores={stores}>
+        <Root dispatcher={dispatcher} stores={stores}>
           {props => <DecoratedComponent {...this.props} {...props} />}
         </Root>
       );
