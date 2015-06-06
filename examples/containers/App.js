df4b1119f899b7b2d9e978ac499d409d3c82f6d4
Dan Abramov <dan.abramov@gmail.com>
2015-06-06 13:39:47 +0300

    Use component in example for consistency

--- examples/containers/App.js
+++ examples/containers/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { dispatch, composeStores } from 'redux';
+import { Dispatcher, composeStores } from 'redux';
 import CounterApp from './CounterApp';
 import TodoApp from './TodoApp';
 import * as stores from '../stores/index';
 
-@dispatch(composeStores(stores))
 export default class App {
   render() {
     return (
-      <div>
-        <CounterApp />
-        <TodoApp />
-      </div>
+      <Dispatcher store={composeStores(stores)}>
+        {() =>
+          <div>
+            <CounterApp />
+            <TodoApp />
+          </div>
+        }
+      </Dispatcher>
     );
   }
 }
