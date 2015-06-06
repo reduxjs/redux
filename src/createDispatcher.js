7a2bd6a516a4697b8b9214b7afb9cd7fa569881d
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2015-06-06 18:57:14 +0300

    createDispatcher should return binded functions

--- src/createDispatcher.js
+++ src/createDispatcher.js
@@ -1,5 +1,12 @@
 import Dispatcher from './Dispatcher';
 
 export default function createDispatcher(...args) {
-  return new Dispatcher(...args);
+  const dispatcher = new Dispatcher(...args);
+
+  return {
+    subscribe: ::dispatcher.subscribe,
+    perform: ::dispatcher.perform,
+    hydrate: ::dispatcher.hydrate,
+    dehydrate: ::dispatcher.dehydrate,
+  };
 }
