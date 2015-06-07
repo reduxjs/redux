65b766fc36937227e51026ed0f5cb7f382ab0cbb
Andrew Clark <acdlite@me.com>
2015-06-07 11:40:07 -0700

    Move CounterApp middleware to global middleware to how it composes with the transactor

--- examples/containers/App.js
+++ examples/containers/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { createDispatcher, Provider, composeStores, reduceStore } from 'redux';
+import { createDispatcher, Provider, composeStores, reduceStore, compose } from 'redux';
 import CounterApp from './CounterApp';
 import TodoApp from './TodoApp';
 import * as stores from '../stores/index';
 import createTransactor from '../middleware/createTransactor';
+import callbackMiddleware from 'redux/middleware/callback';
+
+// Naive implementation of promise middleware
+// Leave full implementation to userland
+function promiseMiddleware(next) {
+  return action =>
+    action && typeof action.then === 'function'
+      ? action.then(next)
+      : next(action);
+}
 
 const store = composeStores(stores);
 const transactor = createTransactor();
-const dispatcher = createDispatcher(getAtom => transactor(getAtom, store));
+const dispatcher = createDispatcher(getAtom => compose(
+  promiseMiddleware,
+  callbackMiddleware,
+  transactor(getAtom, store)
+));
 
 global.transactor = transactor;
 
--- examples/containers/CounterApp.js
+++ examples/containers/CounterApp.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import { connect, bindActionCreators, compose } from 'redux';
+import { connect, bindActionCreators } from 'redux';
 import Counter from '../components/Counter';
 import * as CounterActions from '../actions/CounterActions';
-import callbackMiddleware from 'redux/middleware/callback';
-
-// Naive implementation of promise middleware
-// Leave full implementation to userland
-function promiseMiddleware(next) {
-  return action =>
-    action && typeof action.then === 'function'
-      ? action.then(next)
-      : next(action);
-}
-
-const middleware = compose(callbackMiddleware, promiseMiddleware);
 
 @connect(state => ({
   counter: state.counter
@@ -21,8 +9,7 @@ const middleware = compose(callbackMiddleware, promiseMiddleware);
 export default class CounterApp {
   render() {
     const { counter, dispatcher } = this.props;
-    const dispatch = middleware(dispatcher.dispatch);
-    const actionCreators = bindActionCreators(CounterActions, dispatch);
+    const actionCreators = bindActionCreators(CounterActions, dispatcher.dispatch);
     return (
       <Counter counter={counter}
                {...actionCreators}
