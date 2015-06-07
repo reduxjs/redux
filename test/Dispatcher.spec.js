ee4dadacb02fefe306c0275d4460fd7579ff7316
Nicola Molinari <nicola.molinari@commercetools.de>
2015-06-07 17:23:47 +0200

    Correctly setup dispatcher

--- test/Dispatcher.spec.js
+++ test/Dispatcher.spec.js
@@ -2,6 +2,7 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import chai, { expect } from 'chai';
 import Dispatcher from '../src/Dispatcher';
+import { composeStores } from '../src';
 
 chai.use(sinonChai);
 
@@ -16,17 +17,19 @@ describe('Dispatcher', () => {
   let dispatcher;
 
   beforeEach(() => {
-    dispatcher = new Dispatcher(fakeStore);
+    dispatcher = new Dispatcher(composeStores({ fakeStore }));
   });
 
   it('should correctly initialize', () => {
     expect(dispatcher).to.have.a.property('atom')
+      .that.has.a.property('fakeStore')
       .that.deep.equal(fakeState);
   });
 
   it('should subscribe to changes', done => {
     dispatcher.subscribe(atom => {
-      expect(atom).to.deep.equal(fakeState);
+      expect(atom).to.have.a.property('fakeStore')
+        .that.deep.equal(fakeState);
       done();
     });
   });
@@ -35,7 +38,8 @@ describe('Dispatcher', () => {
     const changeListenerStub = sinon.stub();
     const unsubscribe = dispatcher.subscribe(changeListenerStub);
     expect(changeListenerStub).to.have.callCount(1);
-    expect(changeListenerStub).to.have.been.calledWith(fakeState);
+    expect(changeListenerStub)
+      .to.have.been.calledWith({ fakeStore: fakeState });
     unsubscribe();
     expect(changeListenerStub).to.have.callCount(1);
   });
