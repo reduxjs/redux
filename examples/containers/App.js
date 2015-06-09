d4f0a12a07d80d00a4b6ebf0ebdcc22e43d4cace
Dan Abramov <dan.abramov@gmail.com>
2015-06-09 17:41:34 +0300

    Experiment with uber dispatcher

--- examples/containers/App.js
+++ examples/containers/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import CounterApp from './CounterApp';
 import TodoApp from './TodoApp';
-import { createRedux, Provider } from 'redux';
+import { createRedux, composeStores, Provider } from 'redux';
+import createDispatcher from '../dispatcher/createDispatcher';
 import * as stores from '../stores/index';
 
-const redux = createRedux(stores);
+const store = composeStores(stores);
+// You can modify these options while the code is running:
+const dispatcher = createDispatcher(store, { log: true, replay: false });
+const redux = createRedux(dispatcher);
 
 export default class App {
   render() {
