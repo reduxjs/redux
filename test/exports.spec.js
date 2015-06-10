9d680f52c03b1a46e179a21a80ca314dac59d404
Andrew Clark <acdlite@me.com>
2015-06-10 13:45:41 -0700

    Fix exports test

--- test/exports.spec.js
+++ test/exports.spec.js
@@ -5,16 +5,10 @@ describe('Redux', () => {
 
   it('should export necessary components', () => {
     const imports = Object.keys(redux);
-    expect(imports.length).toBe(9);
+    expect(imports.length).toBe(5);
 
     expect(imports).toContain('createRedux');
     expect(imports).toContain('createDispatcher');
-
-    expect(imports).toContain('Provider');
-    expect(imports).toContain('Connector');
-
-    expect(imports).toContain('provide');
-    expect(imports).toContain('connect');
 
     expect(imports).toContain('compose');
     expect(imports).toContain('composeStores');
