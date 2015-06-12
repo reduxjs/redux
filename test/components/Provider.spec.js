c22cde1ec57d50bd2d628c8ccb90abdcd7555688
Andrew Clark <acdlite@me.com>
2015-06-11 22:10:38 -0700

    Test that Redux does not lose subscribers on hot reload

--- test/components/Provider.spec.js
+++ test/components/Provider.spec.js
@@ -10,18 +10,18 @@ describe('React', () => {
   describe('Provider', () => {
     jsdom();
 
+    class Child extends Component {
+      static contextTypes = {
+        redux: PropTypes.object.isRequired
+      }
+
+      render() {
+        return <div />;
+      }
+    }
+
     it('adds Redux to child context', () => {
       const redux = createRedux({ test: () => 'test' });
-
-      class Child extends Component {
-        static contextTypes = {
-          redux: PropTypes.object.isRequired
-        }
-
-        render() {
-          return <div />;
-        }
-      }
 
       const tree = TestUtils.renderIntoDocument(
         <Provider redux={redux}>
@@ -31,6 +31,36 @@ describe('React', () => {
 
       const child = TestUtils.findRenderedComponentWithType(tree, Child);
       expect(child.context.redux).toBe(redux);
+    });
+
+    it('does not lose subscribers when receiving new props', () => {
+      const redux1 = createRedux({ test: () => 'test' });
+      const redux2 = createRedux({ test: () => 'test' });
+      const spy = expect.createSpy(() => {});
+
+      class ProviderContainer extends Component {
+        state = { redux: redux1 };
+
+        render() {
+          return (
+            <Provider redux={this.state.redux}>
+              {() => <Child />}
+            </Provider>
+          );
+        }
+      }
+
+      const container = TestUtils.renderIntoDocument(<ProviderContainer />);
+      const child = TestUtils.findRenderedComponentWithType(container, Child);
+
+      child.context.redux.subscribe(spy);
+      child.context.redux.dispatch({});
+      expect(spy.calls.length).toEqual(1);
+
+      container.setState({ redux: redux2 });
+      expect(spy.calls.length).toEqual(2);
+      child.context.redux.dispatch({});
+      expect(spy.calls.length).toEqual(3);
     });
   });
 });
