0f9d0b756002503bece43faf2daa72309da1ddc5
Mike Kidder <mikekidder@gmail.com>
2015-06-15 15:46:57 -0500

    cleanup on prepublish.js

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,13 +1,12 @@
 var sh = require('shelljs');
 
 sh.exec('eslint src test', function(code, output) {
-
-  if(code === 0){
+  if(code === 0) {
     sh.exec('npm run clean');
     sh.exec('npm run build');
+    sh.exec('npm run test');
   } else {
     sh.echo('Exit code:', code);
     sh.echo('Program output:', output);
   }
-
 });
