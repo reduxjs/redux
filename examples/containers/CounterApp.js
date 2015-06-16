3a00ff9d62f2ba1100fcc84c6b0cee829067f711
Adam Miskiewicz <adam.skevy@mac.com>
2015-06-16 00:41:55 -0400

    updating examples with new syntax

--- examples/containers/CounterApp.js
+++ examples/containers/CounterApp.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'redux/react';
 import Counter from '../components/Counter';
 import * as CounterActions from '../actions/CounterActions';
 
 @connect(state => ({
   counter: state.counter
-}))
+}), CounterActions)
 export default class CounterApp {
   render() {
-    const { counter, dispatch } = this.props;
+    const { counter, actions } = this.props;
     return (
-      <Counter counter={counter}
-               {...bindActionCreators(CounterActions, dispatch)} />
+      <Counter counter={counter} {...actions} />
     );
   }
 }
--- examples/containers/TodoApp.js
+++ examples/containers/TodoApp.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
 import { Connector } from 'redux/react';
 import AddTodo from '../components/AddTodo';
 import TodoList from '../components/TodoList';
@@ -8,14 +7,13 @@ import * as TodoActions from '../actions/TodoActions';
 export default class TodoApp {
   render() {
     return (
-      <Connector select={state => ({ todos: state.todos })}>
+      <Connector select={state => ({ todos: state.todos })} actionCreators={TodoActions}>
         {this.renderChild}
       </Connector>
     );
   }
 
-  renderChild({ todos, dispatch }) {
-    const actions = bindActionCreators(TodoActions, dispatch);
+  renderChild({ todos, actions }) {
     return (
       <div>
         <AddTodo {...actions} />
