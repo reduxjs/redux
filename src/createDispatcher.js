3932a3d577f3e4fd663645e2feaf93ada81df6c7
Dan Abramov <dan.abramov@gmail.com>
2015-06-17 02:08:40 +0300

    Give the initial action a Symbol type

--- src/createDispatcher.js
+++ src/createDispatcher.js
@@ -1,8 +1,10 @@
 import composeMiddleware from './utils/composeMiddleware';
 
+const INIT = Symbol('INIT');
+
 export default function createDispatcher(store, middlewares = []) {
   return function dispatcher(initialState, setState) {
-    let state = setState(store(initialState, {}));
+    let state = setState(store(initialState, { type: INIT }));
 
     function dispatch(action) {
       state = setState(store(state, action));
