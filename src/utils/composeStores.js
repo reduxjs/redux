185e6529bd8224919f932f2352af70db597778b7
Johannes Lumpe <johannes@johanneslumpe.de>
2015-06-17 14:36:02 +0300

    Ignorning non-func values

--- src/utils/composeStores.js
+++ src/utils/composeStores.js
@@ -1,6 +1,8 @@
 import mapValues from 'lodash/object/mapValues';
+import pick from 'lodash/object/pick';
 
 export default function composeStores(stores) {
+  stores = pick(stores, (val) => typeof val === 'function');
   return function Composition(atom = {}, action) {
     return mapValues(stores, (store, key) =>
       store(atom[key], action)
