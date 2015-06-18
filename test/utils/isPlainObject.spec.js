974b7b8ddf68beee9f73fea5ecb47672e6700888
Franky Chung <franchun@gmail.com>
2015-06-18 16:14:53 +0900

    Check null/undefined case for isPlainObject

--- test/utils/isPlainObject.spec.js
+++ test/utils/isPlainObject.spec.js
@@ -11,6 +11,8 @@ describe('Utils', () => {
       expect(isPlainObject(new Test())).toBe(false);
       expect(isPlainObject(new Date())).toBe(false);
       expect(isPlainObject([1, 2, 3])).toBe(false);
+      expect(isPlainObject(null)).toBe(false);
+      expect(isPlainObject()).toBe(false);
       expect(isPlainObject({ 'x': 1, 'y': 2 })).toBe(true);
     });
   });
