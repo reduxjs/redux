6779eebed5d59c14a58bc0402d63ba66222b0a2d
Ramana Venkata <idlike2dream@gmail.com>
2015-06-19 13:33:34 +0530

    Add counter example back

---
+++ examples/counter/components/Counter.js
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react';
+
+export default class Counter {
+  static propTypes = {
+    increment: PropTypes.func.isRequired,
+    incrementIfOdd: PropTypes.func.isRequired,
+    decrement: PropTypes.func.isRequired,
+    counter: PropTypes.number.isRequired
+  };
+
+  render() {
+    const { increment, incrementIfOdd, decrement, counter } = this.props;
+    return (
+      <p>
+        Clicked: {counter} times
+        {' '}
+        <button onClick={increment}>+</button>
+        {' '}
+        <button onClick={decrement}>-</button>
+        {' '}
+        <button onClick={incrementIfOdd}>Increment if odd</button>
+      </p>
+    );
+  }
+}
---
+++ examples/counter/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      loaders: ['react-hot', 'babel'],
+      exclude: /node_modules/
+    }]
+  }
+};
