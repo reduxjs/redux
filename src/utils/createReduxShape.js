da2fa45775efed620a96e0b5ec15ae0ee56b4622
Nicola Molinari <nicola.molinari@commercetools.de>
2015-06-29 23:38:09 +0200

    More JSDoc

--- src/utils/createReduxShape.js
+++ src/utils/createReduxShape.js
@@ -1,3 +1,9 @@
+/**
+ * Define the shape of the `redux` prop, used for props validation.
+ *
+ * @param  {Object} PropTypes
+ * @return {Object}
+ */
 export default function createReduxShape(PropTypes) {
   return PropTypes.shape({
     subscribe: PropTypes.func.isRequired,
