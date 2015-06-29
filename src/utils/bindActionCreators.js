d96f5f4d9e8a26eb51508d533de00a524e496607
Nicola Molinari <nicola.molinari@commercetools.de>
2015-06-29 22:37:55 +0200

    Add some JSDoc

--- src/utils/bindActionCreators.js
+++ src/utils/bindActionCreators.js
@@ -1,5 +1,13 @@
 import mapValues from '../utils/mapValues';
 
+/**
+ * Given a list action creators, wrap them to the `dispatch` function
+ * in order to be automatically dispatched when invoked.
+ *
+ * @param  {Object} actionCreators - an object with the action functions
+ * @param  {Function} dispatch
+ * @return {Object} the given object with wrapped actions
+ */
 export default function bindActionCreators(actionCreators, dispatch) {
   return mapValues(actionCreators, actionCreator =>
     (...args) => dispatch(actionCreator(...args))
--- src/utils/getDisplayName.js
+++ src/utils/getDisplayName.js
@@ -1,3 +1,9 @@
+/**
+ * Given a React component, return its name to be displayed.
+ *
+ * @param  {React} Component
+ * @return {String} the name of the component
+ */
 export default function getDisplayName(Component) {
   return Component.displayName || Component.name || 'Component';
 }
