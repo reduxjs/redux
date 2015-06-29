b8308ece061acb3be99e8528f3fca19251a122fb
Nicola Molinari <nicola.molinari@commercetools.de>
2015-06-29 23:18:40 +0200

    More JSDoc

--- src/utils/composeStores.js
+++ src/utils/composeStores.js
@@ -1,6 +1,16 @@
 import mapValues from '../utils/mapValues';
 import pick from '../utils/pick';
 
+/**
+ * Given a list of stores, maps the state keys to reducer functions.
+ * The composed store, when invoked, will internally map all the
+ * results of the computed stores (being effectively the state).
+ * When the store function is invoked again, it will pass the
+ * existing state value as initial state.
+ *
+ * @param  {Object} stores - an object with the store reducer functions
+ * @return {Function} the composed store function
+ */
 export default function composeStores(stores) {
   const finalStores = pick(stores, (val) => typeof val === 'function');
   return function Composition(atom = {}, action) {
