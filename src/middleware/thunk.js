d8168470647245891c4f00be1a3ea57b84aaffd5
Nicola Molinari <nicola.molinari@commercetools.de>
2015-06-29 22:37:55 +0200

    Add some JSDoc

--- src/middleware/thunk.js
+++ src/middleware/thunk.js
@@ -1,3 +1,12 @@
+/**
+ * Given an action that returns a function, delegates its call
+ * to the function itself by passing the control of the dispatching,
+ * otherwise simply dispatch the action.
+ * In other words, it allows to have async actions.
+ *
+ * @param  {Function} getState - allow to access the current state
+ * @return {Function} a new middleware
+ */
 export default function thunkMiddleware(getState) {
   return (next) => {
     const recurse = (action) =>
