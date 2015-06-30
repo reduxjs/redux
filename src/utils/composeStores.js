596542314c2b941977ff884a5b8b2a39387cf540
taylorhakes <taylor.hakes@gmail.com>
2015-06-29 22:41:14 -0400

    Throw error on undefined value from store function

--- src/utils/composeStores.js
+++ src/utils/composeStores.js
@@ -1,11 +1,18 @@
-import mapValues from '../utils/mapValues';
-import pick from '../utils/pick';
+import mapValues from './mapValues';
+import pick from './pick';
+
+// UNDEF = undefined;
+const UNDEF = void 0;
 
 export default function composeStores(stores) {
   const finalStores = pick(stores, (val) => typeof val === 'function');
   return function Composition(atom = {}, action) {
-    return mapValues(finalStores, (store, key) =>
-      store(atom[key], action)
-    );
+    return mapValues(finalStores, (store, key) => {
+      const state = store(atom[key], action);
+      if (state === UNDEF) {
+        throw new Error(`Store ${key} returns undefined. By default store should return original state.`);
+      }
+      return state;
+    });
   };
 }
