877c11eb0086c41bec296e93c48fd043d72845cb
taylorhakes <taylor.hakes@gmail.com>
2015-06-30 21:26:59 -0400

    Throw error on undefined value from reducer function

--- src/utils/composeReducers.js
+++ src/utils/composeReducers.js
@@ -5,8 +5,12 @@ export default function composeReducers(reducers) {
   const finalReducers = pick(reducers, (val) => typeof val === 'function');
 
   return function Composition(atom = {}, action) {
-    return mapValues(finalReducers, (store, key) =>
-      store(atom[key], action)
-    );
+    return mapValues(finalReducers, (reducer, key) => {
+      const state = reducer(atom[key], action);
+      if (typeof state === 'undefined') {
+        throw new Error(`Reducer ${key} returns undefined. By default reducer should return original state.`);
+      }
+      return state;
+    });
   };
 }
