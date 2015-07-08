7245b589262c56c6d3b4540c3cd3f17edda65467
Dan Abramov <dan.abramov@gmail.com>
2015-07-08 15:13:30 +0300

    Fix duplicate React when running examples

--- examples/counter/webpack.config.js
+++ examples/counter/webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
   ],
   resolve: {
     alias: {
-      'redux': path.join(__dirname, '../../src')
+      'redux': path.join(__dirname, '..', '..', 'src'),
+      'react': path.join(__dirname, '..', '..', 'node_modules', 'react')
     },
     extensions: ['', '.js']
   },
