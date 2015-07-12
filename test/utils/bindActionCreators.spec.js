18aceeee36d30c4b6928612107408ecf5a908dd7
Dan Abramov <dan.abramov@gmail.com>
2015-07-12 21:06:36 +0300

    Remove nesting from tests

--- test/utils/bindActionCreators.spec.js
+++ test/utils/bindActionCreators.spec.js
@@ -3,29 +3,27 @@ import { bindActionCreators, createStore } from '../../src';
 import { todos } from '../helpers/reducers';
 import * as actionCreators from '../helpers/actionCreators';
 
-describe('Utils', () => {
-  describe('bindActionCreators', () => {
-    let store;
+describe('bindActionCreators', () => {
+  let store;
 
-    beforeEach(() => {
-      store = createStore(todos);
-    });
+  beforeEach(() => {
+    store = createStore(todos);
+  });
 
-    it('should wrap the action creators with the dispatch function', () => {
-      const boundActionCreators = bindActionCreators(actionCreators, store.dispatch);
-      expect(
-        Object.keys(boundActionCreators)
-      ).toEqual(
-        Object.keys(actionCreators)
-      );
+  it('should wrap the action creators with the dispatch function', () => {
+    const boundActionCreators = bindActionCreators(actionCreators, store.dispatch);
+    expect(
+      Object.keys(boundActionCreators)
+    ).toEqual(
+      Object.keys(actionCreators)
+    );
 
-      const action = boundActionCreators.addTodo('Hello');
-      expect(action).toEqual(
-        actionCreators.addTodo('Hello')
-      );
-      expect(store.getState()).toEqual([
-        { id: 1, text: 'Hello' }
-      ]);
-    });
+    const action = boundActionCreators.addTodo('Hello');
+    expect(action).toEqual(
+      actionCreators.addTodo('Hello')
+    );
+    expect(store.getState()).toEqual([
+      { id: 1, text: 'Hello' }
+    ]);
   });
 });
