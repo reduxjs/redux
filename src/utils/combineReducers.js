9045a0c1732ab266a1ff09926420f266e1a74d65
Dan Abramov <dan.abramov@gmail.com>
2015-07-13 19:10:03 +0300

    Handling private actions is an anti-pattern. Enforce it. (Fixes #186)

--- src/utils/combineReducers.js
+++ src/utils/combineReducers.js
@@ -1,19 +1,11 @@
 import mapValues from '../utils/mapValues';
 import pick from '../utils/pick';
 import invariant from 'invariant';
+import { ActionTypes } from '../Store';
 
 function getErrorMessage(key, action) {
   const actionType = action && action.type;
   const actionName = actionType && `"${actionType}"` || 'an action';
-  const reducerName = `Reducer "${key}"`;
-
-  if (actionType === '@@INIT') {
-    return (
-      `${reducerName} returned undefined during initialization. ` +
-      `If the state passed to the reducer is undefined, ` +
-      `you must explicitly return the initial state.`
-    );
-  }
 
   return (
     `Reducer "${key}" returned undefined handling ${actionName}. ` +
@@ -23,6 +15,28 @@ function getErrorMessage(key, action) {
 
 export default function combineReducers(reducers) {
   const finalReducers = pick(reducers, (val) => typeof val === 'function');
+
+  Object.keys(finalReducers).forEach(key => {
+    const reducer = finalReducers[key];
+    invariant(
+      typeof reducer(undefined, { type: ActionTypes.INIT }) !== 'undefined',
+      `Reducer "${key}" returned undefined during initialization. ` +
+      `If the state passed to the reducer is undefined, you must ` +
+      `explicitly return the initial state. The initial state may ` +
+      `not be undefined.`
+    );
+
+    const type = Math.random().toString(36).substring(7).split('').join('.');
+    invariant(
+      typeof reducer(undefined, { type }) !== 'undefined',
+      `Reducer "${key}" returned undefined when probed with a random type. ` +
+      `Don't try to handle ${ActionTypes.INIT} or other actions in "redux/*" ` +
+      `namespace. They are considered private. Instead, you must return the ` +
+      `current state for any unknown actions, unless it is undefined, ` +
+      `in which case you must return the initial state, regardless of the ` +
+      `action type. The initial state may not be undefined.`
+    );
+  });
 
   return function composition(state = {}, action) {
     return mapValues(finalReducers, (reducer, key) => {
