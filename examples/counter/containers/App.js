e3ec5019cfc8d1815290855d995381416dff0c20
Dan Abramov <dan.abramov@gmail.com>
2015-07-13 16:39:41 +0300

    Extract thunk

--- examples/counter/containers/App.js
+++ examples/counter/containers/App.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 import CounterApp from './CounterApp';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import * as reducers from '../reducers';
-
-// TODO: move into a separate project
-function thunk({ dispatch, getState }) {
-  return next => action =>
-    typeof action === 'function' ?
-      action(dispatch, getState) :
-      next(action);
-}
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
