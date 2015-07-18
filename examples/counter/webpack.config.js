fbad31841438c131f3d821e57812a9c6a2648e07
Jay Jaeho Lee <jay@spoqa.com>
2015-07-18 22:41:22 +0900

    Make devtools: source-map for examples

--- examples/counter/webpack.config.js
+++ examples/counter/webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'source-map',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
