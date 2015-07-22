100ce3cd053a1340fe813ccb170f4427a61af2ee
Dan Abramov <dan.abramov@gmail.com>
2015-07-22 19:28:54 +0300

    composeMiddleware is an implementation detail of applyMiddleware

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import compose from './utils/compose';
 import combineReducers from './utils/combineReducers';
 import bindActionCreators from './utils/bindActionCreators';
 import applyMiddleware from './utils/applyMiddleware';
-import composeMiddleware from './utils/composeMiddleware';
 
 export {
   createStore,
   compose,
   combineReducers,
   bindActionCreators,
-  applyMiddleware,
-  composeMiddleware
+  applyMiddleware
 };
