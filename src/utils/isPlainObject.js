be4b589fd76f20d00aacf86c886807df0289baf3
Michal Kvasničák <michal.kvasnicak@gmail.com>
2015-07-23 16:15:22 +0200

    fix isPlainObject, is comparing object stringified object constructors instead of prototypes (did not work from iframes because Object in iframe is not the same as in parent)

--- src/utils/isPlainObject.js
+++ src/utils/isPlainObject.js
@@ -1,12 +1,23 @@
+const fnToString = (fn) => Function.prototype.toString.call(fn);
+
 /**
  * @param {any} obj The object to inspect.
  * @returns {boolean} True if the argument appears to be a plain object.
  */
 export default function isPlainObject(obj) {
-  if (!obj) {
+  if (!obj || typeof obj !== 'object') {
     return false;
   }
 
-  return typeof obj === 'object' &&
-         Object.getPrototypeOf(obj) === Object.prototype;
+  const proto = typeof obj.constructor === 'function' ? Object.getPrototypeOf(obj) : Object.prototype;
+
+  if (proto === null) {
+    return true;
+  }
+
+  var constructor = proto.constructor;
+
+  return typeof constructor === 'function'
+    && constructor instanceof constructor
+    && fnToString(constructor) === fnToString(Object);
 }
