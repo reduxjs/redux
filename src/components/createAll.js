df18c6d3ab273e75b5d7a3c080daade08cff84e0
gnoff <jcs.gnoff@gmail.com>
2015-07-31 00:24:24 -0700

    remvoing deprecated code for Connector and original connectDecorator

--- src/components/createAll.js
+++ src/components/createAll.js
@@ -1,19 +1,15 @@
 import createProvider from './createProvider';
 import createProvideDecorator from './createProvideDecorator';
 
-import createConnector from './createConnector';
-import createConnectDecoratorDeprecated from './createConnectDecoratorDeprecated';
 import createConnectDecorator from './createConnectDecorator';
 
 export default function createAll(React) {
   // Wrapper components
   const Provider = createProvider(React);
-  const Connector = createConnector(React);
 
   // Higher-order components (decorators)
   const provide = createProvideDecorator(React, Provider);
-  const connectDeprecated = createConnectDecoratorDeprecated(React, Connector);
   const connect = createConnectDecorator(React);
 
-  return { Provider, Connector, provide, connect, connectDeprecated };
+  return { Provider, provide, connect };
 }
