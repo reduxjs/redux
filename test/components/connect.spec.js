4fa375d0edcb9f2041901312190df146a7239c8a
cybice <istarkov@gmail.com>
2015-08-11 22:00:18 +0300

    Add failing test

--- test/components/connect.spec.js
+++ test/components/connect.spec.js
@@ -222,6 +222,98 @@ describe('React', () => {
       };
 
       expect(propsBefore.x).toEqual(true);
+      expect('x' in propsAfter).toEqual(false, 'x prop must be removed');
+    });
+
+    it('should remove undefined props without mapDispatchToProps', () => {
+      const store = createStore(() => ({}));
+      let props = { x: true };
+      let container;
+
+      @connect(() => ({}))
+      class ConnectContainer extends Component {
+        render() {
+          return (
+              <div {...this.props} />
+          );
+        }
+      }
+
+      class HolderContainer extends Component {
+        render() {
+          return (
+            <ConnectContainer {...props} />
+          );
+        }
+      }
+
+      TestUtils.renderIntoDocument(
+        <Provider store={store}>
+          {() => (
+            <HolderContainer ref={instance => container = instance} />
+          )}
+        </Provider>
+      );
+
+      const propsBefore = {
+        ...TestUtils.findRenderedDOMComponentWithTag(container, 'div').props
+      };
+
+      props = {};
+      container.forceUpdate();
+
+      const propsAfter = {
+        ...TestUtils.findRenderedDOMComponentWithTag(container, 'div').props
+      };
+
+      expect(propsBefore.x).toEqual(true);
+      expect('x' in propsAfter).toEqual(false, 'x prop must be removed');
+    });
+
+    it('should remove undefined props without mapDispatchToProps', () => {
+      // in this failing test connect doesn't call render second time
+      // i can't understand why
+      const store = createStore(() => ({}));
+      let props = { x: true };
+      let container;
+
+      @connect(() => ({}))
+      class ConnectContainer extends Component {
+        render() {
+          return (
+              <div {...this.props} />
+          );
+        }
+      }
+
+      class HolderContainer extends Component {
+        render() {
+          return (
+            <ConnectContainer {...props} />
+          );
+        }
+      }
+
+      TestUtils.renderIntoDocument(
+        <Provider store={store}>
+          {() => (
+            <HolderContainer ref={instance => container = instance} />
+          )}
+        </Provider>
+      );
+
+      const propsBefore = {
+        ...TestUtils.findRenderedDOMComponentWithTag(container, 'div').props
+      };
+
+      props = {};
+      container.forceUpdate();
+
+      const propsAfter = {
+        ...TestUtils.findRenderedDOMComponentWithTag(container, 'div').props
+      };
+
+      expect(propsBefore.x).toEqual(true);
       expect(propsAfter.x).toNotEqual(true);
     });
 
