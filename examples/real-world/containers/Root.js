948bb11ad65a801ea10b597cbcdc1cc6631aebb0
Forbes Lindesay <forbes@lindesay.co.uk>
2015-08-16 02:35:52 +0100

    Pass store as prop to Root component in real-world example

--- examples/real-world/containers/Root.js
+++ examples/real-world/containers/Root.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router';
-import configureStore from '../store/configureStore';
 import App from './App';
 import UserPage from './UserPage';
 import RepoPage from './RepoPage';
-
-const store = configureStore();
 
 export default class Root extends Component {
   render() {
     return (
       <div>
-        <Provider store={store}>
+        <Provider store={this.props.store}>
           {() =>
             <Router history={this.props.history}>
               <Route path='/' component={App}>
