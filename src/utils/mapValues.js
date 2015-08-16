76a52440e4a768f238a318c6530abdd45f6bbe6a
George Lifchits <george.lifchits@gmail.com>
2015-08-16 00:59:04 -0400

    Update mapValues.js
    small typo in JSDoc

--- src/utils/mapValues.js
+++ src/utils/mapValues.js
@@ -2,7 +2,7 @@
  * Applies a function to every key-value pair inside an object.
  *
  * @param {Object} obj The source object.
- * @param {Function} fn The mapper function taht receives the value and the key.
+ * @param {Function} fn The mapper function that receives the value and the key.
  * @returns {Object} A new object that contains the mapped values for the keys.
  */
 export default function mapValues(obj, fn) {
