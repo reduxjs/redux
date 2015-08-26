8a86259f6134d5c099093903da2c94adbe472141
Tim Dorr <timdorr@timdorr.com>
2015-08-25 23:09:40 -0400

    Add some better documentation to the async counter

--- examples/universal/api/counter.js
+++ examples/universal/api/counter.js
@@ -3,7 +3,11 @@ function getRandomInt(min, max) {
 }
 
 export function fetchCounter(callback) {
+  // Rather than immediately returning, we delay our code with a timeout to simulate asynchronous behavior
   setTimeout(() => {
     callback(getRandomInt(1, 100));
   }, 500);
+  
+  // In the case of a real world API call, you'll normally run into a Promise like this:
+  // API.getUser().then(user => callback(user));
 }
