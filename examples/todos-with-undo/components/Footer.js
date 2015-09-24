24fcf3eee3cb2839099fe546ce3af89871d3a4b1
Daniel Bugl <daniel.bugl@touchlay.com>
2015-09-24 15:10:38 +0200

    Add todos-with-undo eample

---
+++ examples/todos-with-undo/components/Footer.js
@@ -0,0 +1,64 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Footer extends Component {
+  renderFilter(filter, name) {
+    if (filter === this.props.filter) {
+      return name;
+    }
+
+    return (
+      <a href="#" onClick={e => {
+        e.preventDefault();
+        this.props.onFilterChange(filter);
+      }}>
+        {name}
+      </a>
+    );
+  }
+
+  renderFilters() {
+    return (
+      <p>
+        Show:
+        {' '}
+        {this.renderFilter('SHOW_ALL', 'All')}
+        {', '}
+        {this.renderFilter('SHOW_COMPLETED', 'Completed')}
+        {', '}
+        {this.renderFilter('SHOW_ACTIVE', 'Active')}
+        .
+      </p>
+    );
+  }
+
+  renderUndo() {
+    return (
+      <p>
+        <button onClick={this.props.onUndo} disabled={this.props.undoDisabled}>Undo</button>
+        <button onClick={this.props.onRedo} disabled={this.props.redoDisabled}>Redo</button>
+      </p>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderFilters()}
+        {this.renderUndo()}
+      </div>
+    );
+  }
+}
+
+Footer.propTypes = {
+  onFilterChange: PropTypes.func.isRequired,
+  onUndo: PropTypes.func.isRequired,
+  onRedo: PropTypes.func.isRequired,
+  undoDisabled: PropTypes.bool.isRequired,
+  redoDisabled: PropTypes.bool.isRequired,
+  filter: PropTypes.oneOf([
+    'SHOW_ALL',
+    'SHOW_COMPLETED',
+    'SHOW_ACTIVE'
+  ]).isRequired
+};
---
+++ examples/todos-with-undo/components/TodoList.js
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+import Todo from './Todo';
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map((todo, index) =>
+          <Todo {...todo}
+                key={index}
+                onClick={() => this.props.onTodoClick(index)} />
+        )}
+      </ul>
+    );
+  }
+}
+
+TodoList.propTypes = {
+  onTodoClick: PropTypes.func.isRequired,
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired
+  }).isRequired).isRequired
+};
