7b834d990d285731cec5f8a2094d4678dbb98844
Leonardo Andres Garcia Crespo <leoasis@gmail.com>
2015-10-02 14:07:32 +0200

    connect correctly typed. Yayyyyy

--- examples/flow-counter/containers/App.js
+++ examples/flow-counter/containers/App.js
@@ -2,11 +2,9 @@
 
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
-import type { CounterProps } from '../components/Counter';
 import { increment, decrement, incrementIfOdd, incrementAsync } from '../actions/counter';
-import { AppState, Dispatchable } from '../types';
+import type { AppState, Dispatchable } from '../types';
 import type { Dispatch } from 'redux';
-import type { Component } from 'react';
 
 type StateProps = AppState;
 
@@ -25,12 +23,11 @@ type DispatchProps = {
 
 function mapDispatchToProps(dispatch: Dispatch<Dispatchable>) : DispatchProps {
   return {
-    increment: (...args) => dispatch(increment(...args)),
-    decrement: (...args) => dispatch(decrement(...args)),
-    incrementIfOdd: (...args) => dispatch(incrementIfOdd(...args)),
-    incrementAsync: (...args) => dispatch(incrementAsync(...args)),
+    increment: () => dispatch(increment()),
+    decrement: () => dispatch(decrement()),
+    incrementIfOdd: () => dispatch(incrementIfOdd()),
+    incrementAsync: () => dispatch(incrementAsync()),
   };
 }
-// TODO: Cannot make this to correctly type check that the resulting props match
-// the Counter prop types
+
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- examples/flow-counter/interfaces/react-redux.js
+++ examples/flow-counter/interfaces/react-redux.js
@@ -1,14 +1,15 @@
 import type { Component } from 'react';
 import type { Dispatch, Store } from 'redux';
 
+
 declare module 'react-redux' {
   declare type MapStateToProps<State, Props> = (state: State) => Props;
   declare type MapDispatchToProps<Dispatchable, Props> = (dispatch: Dispatch<Dispatchable>) => Props;
 
-  declare function connect<State, Dispatchable, ComponentProps, DefaultProps, ComponentState, StateProps, DispatchProps: $Diff<ComponentProps, StateProps>>(
+  declare function connect<State, Dispatchable, StateProps, ComponentProps, DispatchProps: $Diff<ComponentProps, StateProps>, ComponentDefaultProps, ComponentState>(
     mapStateToProps: MapStateToProps<State, StateProps>,
     mapDispatchToProps: MapDispatchToProps<Dispatchable, DispatchProps>
-  ) : (component: Component<DefaultProps, ComponentProps, ComponentState>) => Component<DefaultProps, ComponentProps, ComponentState>;
+  ) : (component: ReactClass<ComponentDefaultProps, ComponentProps, ComponentState>) => ReactClass<ComponentDefaultProps, ComponentProps, ComponentState>;
 
   declare type ProviderProps<S, D> = {
     // TODO: For some super weird reason I need to use this global declaration of the store,
