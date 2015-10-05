50877622581f06a40e465aa16bf4ab2e6f52498a
Bonnie DiPasquale <bvasko@gmail.com>
2015-10-05 11:29:17 -0400

    Update babel config for new transform format
    Update the babel-plugin-react-transform config format to the new,
    non-deprecated format from upstream. (v1.1.0+)

--- examples/universal/webpack.config.js
+++ examples/universal/webpack.config.js
@@ -32,11 +32,13 @@ module.exports = {
               'react-transform'
             ],
             extra: {
-              'react-transform': [{
-                target:  'react-transform-hmr',
-                imports: ['react'],
-                locals:  ['module']
-              }]
+              'react-transform': {
+                transforms: [{
+                  transform:  'react-transform-hmr',
+                  imports: ['react'],
+                  locals:  ['module']
+                }]
+              }
             }
           }
         }
