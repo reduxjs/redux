9e643e4db8b25cd7581867a1be2160dd0ed60859
Harry <hedgerh@gmail.com>
2015-10-05 02:54:07 -0400

    Updated Shop API

--- examples/shopping-cart/api/shop.js
+++ examples/shopping-cart/api/shop.js
@@ -1,22 +1,16 @@
 /**
  * Mocking client-server processing
  */
-var Shop = exports;
+import _products from './products.json';
 
-var _products = require('./products.json');
+const TIMEOUT = 100;
 
-var TIMEOUT = 100;
-
-Shop.getProducts = function(cb, timeout) {
-  timeout = timeout || TIMEOUT;
-  setTimeout(function() {
-    cb(_products);
-  }, timeout);
-};
-
-Shop.buyProducts = function(payload, cb, timeout) {
-  timeout = timeout || TIMEOUT;
-  setTimeout(function() {
-    cb();
-  }, timeout);
+export default const Shop = {
+  getProducts(cb, timeout) {
+    setTimeout(() => cb(_products), timeout || TIMEOUT);
+  },
+  
+  buyProducts(payload, cb, timeout) {
+    setTimeout(() => cb(), timeout || TIMEOUT)
+  }
 };
