e9e4c568077016e0884c9656baf16432fd25d90c
Lee Bannard <l_bannard@yahoo.co.uk>
2015-10-26 22:06:40 +0000

    Fix lint errors in examples/async/

--- examples/async/components/Posts.js
+++ examples/async/components/Posts.js
@@ -1,4 +1,4 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes, Component } from 'react'
 
 export default class Posts extends Component {
   render() {
@@ -8,10 +8,10 @@ export default class Posts extends Component {
           <li key={i}>{post.title}</li>
         )}
       </ul>
-    );
+    )
   }
 }
 
 Posts.propTypes = {
   posts: PropTypes.array.isRequired
-};
+}
