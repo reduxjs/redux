422c37ac74434b6436f166f13cf254999932bbb9
Brent Van Minnen <bjvanminnen@gmail.com>
2015-11-27 13:08:19 -0800

    remove semicolons

--- src/utils/compose.js
+++ src/utils/compose.js
@@ -9,12 +9,12 @@ export default function compose(...funcs) {
   return (...args) => {
     if (funcs.length === 0) {
       // We weren't given any functions, just return the first passed in arg.
-      return args[0];
+      return args[0]
     }
 
-    const last = funcs[funcs.length - 1];
-    const rest = funcs.slice(0, -1);
+    const last = funcs[funcs.length - 1]
+    const rest = funcs.slice(0, -1)
 
-    return rest.reduceRight((composed, f) => f(composed), last(...args));
+    return rest.reduceRight((composed, f) => f(composed), last(...args))
   }
 }
