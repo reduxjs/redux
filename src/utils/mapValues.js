0956991543062f10211d5b8ec63088a94686d8c0
sebastien <lorber.sebastien@gmail.com>
2015-11-29 01:05:37 +0100

    mapValues: returns input object when called with identity function

--- src/utils/mapValues.js
+++ src/utils/mapValues.js
@@ -3,11 +3,16 @@
  *
  * @param {Object} obj The source object.
  * @param {Function} fn The mapper function that receives the value and the key.
- * @returns {Object} A new object that contains the mapped values for the keys.
+ * @returns {Object} A new object that contains the mapped values for the keys,
+ * or the input object if no modifications were performed
  */
-export default function mapValues(obj, fn) {
-  return Object.keys(obj).reduce((result, key) => {
-    result[key] = fn(obj[key], key)
+export default function mapValues(obj, fn,toto) {
+  var hasChanged = false
+  var result = Object.keys(obj).reduce((result, key) => {
+    var mappedValue = fn(obj[key], key)
+    hasChanged = hasChanged || ( mappedValue !== obj[key] )
+    result[key] = mappedValue
     return result
   }, {})
+  return hasChanged ? result : obj;
 }
