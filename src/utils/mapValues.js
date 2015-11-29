717f60a776dc418be39984a13cccbf0bb601c886
sebastien <lorber.sebastien@gmail.com>
2015-11-29 01:54:24 +0100

    remove typo in method signature + fix doc

--- src/utils/mapValues.js
+++ src/utils/mapValues.js
@@ -4,9 +4,9 @@
  * @param {Object} obj The source object.
  * @param {Function} fn The mapper function that receives the value and the key.
  * @returns {Object} A new object that contains the mapped values for the keys,
- * or the input object if no modifications were performed
+ * or the source object if no modifications were performed
  */
-export default function mapValues(obj, fn,toto) {
+export default function mapValues(obj, fn) {
   var hasChanged = false
   var result = Object.keys(obj).reduce((result, key) => {
     var mappedValue = fn(obj[key], key)
