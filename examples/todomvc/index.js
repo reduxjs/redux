2b02e1be5e4d8390b876bf90aa63c4a7b2034acb
Graham Mendick <graham.mendick@gmail.com>
2016-01-06 14:42:53 +0000

    Added StateInfo configuration

--- examples/todomvc/index.js
+++ examples/todomvc/index.js
@@ -5,12 +5,26 @@ import { Provider } from 'react-redux'
 import App from './containers/App'
 import configureStore from './store/configureStore'
 import 'todomvc-app-css/index.css'
+import { start, StateInfoConfig } from 'navigation'
+
+StateInfoConfig.build([
+    { key: 'todomvc', initial: 'app', states: [
+        { key: 'app', route: '{filter?}' }
+    ]}   
+]);
 
 const store = configureStore()
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+var todomvc = StateInfoConfig.dialogs.todomvc;
+todomvc.states.app.navigated =  () => {
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById('root')
+    );    
+}
+
+start();
+
+
