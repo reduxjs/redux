f357dfd004b23f79f0120931a9905b3b0025b139
Graham Mendick <graham.mendick@gmail.com>
2016-01-08 10:21:14 +0000

    Used constant for default filter value

--- examples/todomvc/routes.js
+++ examples/todomvc/routes.js
@@ -1,9 +1,10 @@
 import { StateInfoConfig } from 'navigation'
+import { SHOW_ALL } from './constants/TodoFilters'
 
 function configureStateInfo() {
   StateInfoConfig.build([
     { key: 'todomvc', initial: 'app', states: [
-      { key: 'app', route: '{filter?}', defaults: { filter: 'all' }, trackCrumbTrail: false }
+      { key: 'app', route: '{filter?}', defaults: { filter: SHOW_ALL }, trackCrumbTrail: false }
     ] }   
   ])
 }
