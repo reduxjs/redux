d2969b5e5a1fcc1489feec0a4fcc06f92e1a3e6b
Tim Dorr <timdorr@timdorr.com>
2016-01-12 22:46:47 -0500

    Tweak messaging a tiny bit :ok_hand:

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import applyMiddleware from './applyMiddleware'
 import compose from './compose'
 
 /*
-* create a function so that we can check if the function name has been altered by minification
-* if the function has been minified and NODE_ENV !== 'production', warn the user
+* This is a dummy function to check if the function name has been altered by minification.
+* If the function has been minified and NODE_ENV !== 'production', warn the user
 */
 function isCrushed() {}
 
 if (isCrushed.name !== 'isCrushed' && process.env.NODE_ENV !== 'production') {
   /*eslint-disable no-console */
   console.error('You are currently using minified code outside of NODE_ENV === \'production\'. ' + 
-    'This means that you are running a slower development only build of Redux. ' + 
-    'Consult tools such as loose-envify (https://github.com/zertosh/loose-envify) for browserify ' + 
-    'and DefinePlugin for webpack (http://stackoverflow.com/questions/30030031) ' + 
-    'to build with proper NODE_ENV') 
+    'This means that you are running a slower development build of Redux. ' + 
+    'You can use loose-envify (https://github.com/zertosh/loose-envify) for browserify ' + 
+    'or DefinePlugin for webpack (http://stackoverflow.com/questions/30030031) ' + 
+    'to ensure you have the correct code for your production build.') 
   /*eslint-enable */
 }
 
