7489e5796b79db5383cf196b05243f4ac5486395
Tim Dorr <timdorr@timdorr.com>
2016-01-13 20:17:10 -0500

    Even tinier tweaks. :microscope:

--- src/index.js
+++ src/index.js
@@ -12,11 +12,13 @@ function isCrushed() {}
 
 if (isCrushed.name !== 'isCrushed' && process.env.NODE_ENV !== 'production') {
   /*eslint-disable no-console */
-  console.error('You are currently using minified code outside of NODE_ENV === \'production\'. ' + 
+  console.error(
+    'You are currently using minified code outside of NODE_ENV === \'production\'. ' + 
     'This means that you are running a slower development build of Redux. ' + 
     'You can use loose-envify (https://github.com/zertosh/loose-envify) for browserify ' + 
     'or DefinePlugin for webpack (http://stackoverflow.com/questions/30030031) ' + 
-    'to ensure you have the correct code for your production build.') 
+    'to ensure you have the correct code for your production build.'
+  ) 
   /*eslint-enable */
 }
 
