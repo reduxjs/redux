eab1162b9f825c5f60b5de56f1d6cbe9927b8f93
Pierrickouw <pierrick.wauquier@gmail.com>
2016-01-25 22:23:19 +0100

    Reconcile router configuration on containers

--- examples/real-world/containers/Root.dev.js
+++ examples/real-world/containers/Root.dev.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { Provider } from 'react-redux'
-import { ReduxRouter } from 'redux-router'
+import Routes from '../routes'
 import DevTools from './DevTools'
 
 export default class Root extends Component {
@@ -9,7 +9,7 @@ export default class Root extends Component {
     return (
       <Provider store={store}>
         <div>
-          <ReduxRouter />
+          <Routes />
           <DevTools />
         </div>
       </Provider>
--- examples/real-world/containers/Root.prod.js
+++ examples/real-world/containers/Root.prod.js
@@ -1,13 +1,14 @@
 import React, { Component, PropTypes } from 'react'
 import { Provider } from 'react-redux'
-import { ReduxRouter } from 'redux-router'
+
+import Routes from '../routes'
 
 export default class Root extends Component {
   render() {
     const { store } = this.props
     return (
       <Provider store={store}>
-        <ReduxRouter />
+        <Routes />
       </Provider>
     )
   }
