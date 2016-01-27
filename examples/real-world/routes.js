aec8ca40dc49600b3b5c068bcab4b6a2394e9c61
tinybug <jesselfree@gmail.com>
2016-01-27 18:11:03 +0800

    keep the routes in JSX

--- examples/real-world/routes.js
+++ examples/real-world/routes.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import { Route, browserHistory, Router } from 'react-router'
+import { Route } from 'react-router'
 import App from './containers/App'
 import UserPage from './containers/UserPage'
 import RepoPage from './containers/RepoPage'
 
-export default function Routes() {
-  return (
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <Route path="/:login/:name"
-               component={RepoPage} />
-        <Route path="/:login"
-               component={UserPage} />
-      </Route>
-    </Router>
-  )
-}
+export default (
+    <Route path="/" component={App}>
+      <Route path="/:login/:name"
+             component={RepoPage} />
+      <Route path="/:login"
+             component={UserPage} />
+    </Route>
+)
