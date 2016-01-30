f9397a1dd9ed2f6fb730eff36cd90651d34ae4e7
Muneeb <neebz@users.noreply.github.com>
2016-01-30 13:13:42 +0400

    fixed the if clause

--- src/createStore.js
+++ src/createStore.js
@@ -159,11 +159,9 @@ export default function createStore(reducer, initialState, enhancer) {
    * @returns {void}
    */
   function replaceReducer(nextReducer) {
-    if (typeof nextReducer !== 'undefined') {
       if (typeof nextReducer !== 'function') {
-        throw new Error('Expected the nextReducer to be a function.')
+        throw new Error('Expected the reducer to be a function.')
       }
-    }
     currentReducer = nextReducer
     dispatch({ type: ActionTypes.INIT })
   }
