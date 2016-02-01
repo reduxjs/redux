e7295c33776be6199b826817934dadad5d0f9bb1
John-David Dalton <john.david.dalton@gmail.com>
2016-01-31 17:11:59 -0800

    Use lodash v4.

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,18 @@ var config = {
     libraryTarget: 'umd'
   },
   plugins : [
+    {
+      apply: function apply(compiler) {
+        compiler.parser.plugin('expression global', function expressionGlobalPlugin() {
+          this.state.module.addVariable('global', "(function() { return this; }()) || Function('return this')()")
+          return false
+        });
+      }
+    },
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env)
-    })    
+    })
   ]
 };
 
