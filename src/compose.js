a7f0a923bbe881d7052af4536a4b5ffb8ae23af8
Krawaller <info@krawaller.se>
2016-02-14 13:35:46 +0100

    Clarify comment in compose.js
    This PR updates the documentation comment in `compose.js` to clarify that the rightmost function can have multiple arguments, as it provides the signature for the resulting composite function.

--- src/compose.js
+++ src/compose.js
@@ -1,10 +1,14 @@
 /**
- * Composes single-argument functions from right to left.
+ * Composes single-argument functions from right to left. The rightmost
+ * function can take multiple arguments as it provides the signature for
+ * the resulting composite function.
  *
  * @param {...Function} funcs The functions to compose.
- * @returns {Function} A function obtained by composing functions from right to
- * left. For example, compose(f, g, h) is identical to arg => f(g(h(arg))).
+ * @returns {Function} A function obtained by composing the argument functions
+ * from right to left. For example, compose(f, g, h) is identical to doing
+ * (...args) => f(g(h(...args))).
  */
+ 
 export default function compose(...funcs) {
   return (...args) => {
     if (funcs.length === 0) {
