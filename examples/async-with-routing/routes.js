25cfc5ab66d9614692c7ce01788ece234248aafa
xulien <julien@brunet.io>
2016-02-17 22:31:37 +0100

    cleaning

--- examples/async-with-routing/routes.js
+++ examples/async-with-routing/routes.js
@@ -8,11 +8,11 @@ import Home from './components/Home'
 import Sample from './components/Sample'
 import Reddit from './containers/Reddit'
 
-export default (
-      <Route path="/" component={Layout}>
-        <IndexRoute component={Home} />
-        <Route path="sample" component={Sample} />
-        <Redirect from="reddit" to="reddit/reactjs" />
-        <Route path="reddit/:id" component={Reddit} />
-      </Route>
-  )
+export default(
+  <Route path="/" component={Layout}>
+    <IndexRoute component={Home}/>
+    <Route path="sample" component={Sample}/>
+    <Redirect from="reddit" to="reddit/reactjs"/>
+    <Route path="reddit/:id" component={Reddit}/>
+  </Route>
+)
