6b61fca446bf1a330f92ad549b6f22e69c57e3ae
Daniel Lytkin <gaelco.salmon@gmail.com>
2016-02-18 23:07:56 +0600

    Fix linter error

--- test/typescript.spec.js
+++ test/typescript.spec.js
@@ -1,7 +1,7 @@
 import * as tt from 'typescript-definition-tester'
 
 
-describe('TypeScript definitions', function() {
+describe('TypeScript definitions', function () {
   this.timeout(0)
 
   it('should compile against index.d.ts', (done) => {
