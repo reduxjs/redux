f7dbb53eaa6c467a67696b63e6837ae6ced2aab4
Raja Rao DV <rajaraodv100@yahoo.com>
2016-02-22 18:21:15 -0800

    Promote AddTodo to proper container component

---
+++ examples/todos/components/AddTodoForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+let AddTodoForm = ({ onSubmit }) => {
+  let input
+
+  return (
+    <div>
+      <form onSubmit={e => {
+        e.preventDefault()
+        if (!input.value.trim()) {
+          return
+        }
+        onSubmit(input.value)
+        input.value = ''
+      }}>
+        <input ref={node => {
+          input = node
+        }} />
+        <button type="submit">
+          Add Todo
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default AddTodoForm
