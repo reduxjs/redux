3abac2160483c653be641ecc01e50b71903292eb
Dan Abramov <dan.abramov@gmail.com>
2016-02-28 16:57:01 +0000

    RFC: remove React Transform from examples

--- examples/async/index.js
+++ examples/async/index.js
@@ -1,15 +1,34 @@
 import 'babel-polyfill'
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import App from './containers/App'
 import configureStore from './store/configureStore'
+import App from './containers/App'
 
 const store = configureStore()
+const rootEl = document.getElementById('root')
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+function render(rootReactEl) {
+  ReactDOM.render(
+    <Provider store={store}>
+      {rootReactEl}
+    </Provider>,
+    rootEl
+  )
+}
+
+render(<App />)
+
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    const RedBox = require('redbox-react')
+    const UpdatedApp = require('./containers/App').default
+    setTimeout(() => {
+      try {
+        render(<UpdatedApp />)
+      } catch (error) {
+        render(<RedBox error={error} />)
+      }
+    })
+  })
+}
