0b9416bb8d833fa246d21e16f346e75547d86290
Dan Abramov <dan.abramov@gmail.com>
2016-03-06 04:30:34 +0000

    Remove hacks to alias Redux to src folder in examples
    We don’t actively develop the source anymore for this to be useful.
    This only creates confusion and, additionally, breaks hot reloading because of BABEL_ENV conflicts with the root .babelrc.

--- examples/todomvc/webpack.config.js
+++ examples/todomvc/webpack.config.js
@@ -33,22 +33,3 @@ module.exports = {
     ]
   }
 }
-
-
-// When inside Redux repo, prefer src to compiled version.
-// You can safely delete these lines in your project.
-var reduxSrc = path.join(__dirname, '..', '..', 'src')
-var reduxNodeModules = path.join(__dirname, '..', '..', 'node_modules')
-var fs = require('fs')
-if (fs.existsSync(reduxSrc) && fs.existsSync(reduxNodeModules)) {
-  // Resolve Redux to source
-  module.exports.resolve = { alias: { 'redux': reduxSrc } }
-  // Our root .babelrc needs this flag for CommonJS output
-  process.env.BABEL_ENV = 'commonjs'
-  // Compile Redux from source
-  module.exports.module.loaders.push({
-    test: /\.js$/,
-    loaders: [ 'babel' ],
-    include: reduxSrc
-  })
-}
