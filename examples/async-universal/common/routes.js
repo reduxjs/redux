c91dccb70369d61259765cb08e3d239bda4b7bf1
xulien <julien@brunet.io>
2016-03-11 09:33:31 +0100

    Layout.js removed

--- examples/async-universal/common/routes.js
+++ examples/async-universal/common/routes.js
@@ -2,12 +2,10 @@ import React from 'react'
 import Route from 'react-router/lib/Route'
 import IndexRoute from 'react-router/lib/IndexRoute'
 
-import Layout from './components/Layout'
 import Reddit from './containers/Reddit'
 
 export default(
-  <Route path="/" component={Layout}>
-    <IndexRoute component={Reddit}/>
+  <Route path="/" component={Reddit}>
     <Route path=":id" component={Reddit}/>
   </Route>
 )
