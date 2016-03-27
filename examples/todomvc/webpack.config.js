ead1df94c1cbc9b7ef35a22ad2cda20f28861e52
just-boris <just-boris@hotmail.com>
2016-03-28 01:05:15 +0300

    avoid npm install for each example to make build faster

--- examples/todomvc/webpack.config.js
+++ examples/todomvc/webpack.config.js
@@ -26,8 +26,7 @@ module.exports = {
       },
       {
         test: /\.css?$/,
-        loaders: [ 'style', 'raw' ],
-        include: __dirname
+        loaders: [ 'style', 'raw' ]
       }
     ]
   }
