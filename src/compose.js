c7d57bce8353165b79673b9e4fbaf41a1b722ceb
aimwhy <aimwhy@foxmail.com>
2016-04-10 07:00:50 +0800

    Update compose.js

--- src/compose.js
+++ src/compose.js
@@ -10,12 +10,11 @@
  */
  
 export default function compose(...funcs) {
-  if (funcs.length) {
-  	const last = funcs[funcs.length - 1];
-  	const rest = funcs.slice(0, -1);
-  	
-  	return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args));
-  } else {
+  if(funcs.length){
+    const last = funcs[funcs.length - 1];
+    const rest = funcs.slice(0, -1);
+    
+    return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args));
+  }else{
     return (...args) => args[0];
   }
-}
