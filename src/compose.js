56f8c7f39d428c58a7dbeb9683ea051e0b649eb5
aimwhy <aimwhy@foxmail.com>
2016-04-12 03:40:11 +0800

    Avoid every execution and allocation (#1595)
    * Avoid every execution and allocation
    
    * Update compose.js
    
    * Update compose.js
    
    * Update compose.js
    
    * Update compose.js
    
    * Update compose.js

--- src/compose.js
+++ src/compose.js
@@ -8,16 +8,13 @@
  * from right to left. For example, compose(f, g, h) is identical to doing
  * (...args) => f(g(h(...args))).
  */
- 
-export default function compose(...funcs) {
-  return (...args) => {
-    if (funcs.length === 0) {
-      return args[0]
-    }
 
+export default function compose(...funcs) {
+  if (!funcs.length) {
+    return (...args) => args[0]
+  } else {
     const last = funcs[funcs.length - 1]
     const rest = funcs.slice(0, -1)
-
-    return rest.reduceRight((composed, f) => f(composed), last(...args))
+    return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args))
   }
 }
