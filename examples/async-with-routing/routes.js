bf3b17a8b93dedfefbff5e5804173b3132fcc371
xulien <julien@brunet.io>
2016-04-12 13:12:22 +0200

    Layout.js renamed to App.js

--- examples/async-with-routing/routes.js
+++ examples/async-with-routing/routes.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Route from 'react-router/lib/Route'
 import IndexRoute from 'react-router/lib/IndexRoute'
 
-import Layout from './components/Layout'
+import App from './components/App'
 import Reddit from './containers/Reddit'
 
 export default(
-  <Route path="/" component={Layout}>
+  <Route path="/" component={App}>
     <IndexRoute component={Reddit}/>
     <Route path=":id" component={Reddit}/>
   </Route>
