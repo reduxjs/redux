d4604bb3a5c3922f1dead96a0a19549333d36bec
xulien <julien@brunet.io>
2016-04-12 09:57:13 +0200

    typo

--- examples/async-universal/server/server.js
+++ examples/async-universal/server/server.js
@@ -26,10 +26,7 @@ app.use(webpackHotMiddleware(compiler))
 app.use(handleRender)
 
 function handleRender(req, res) {
-  match({
-    routes,
-    location: req.url
-  }, (error, redirectLocation, renderProps) => {
+  match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
     if (error) {
       res.status(500).send(error.message)
     } else if (redirectLocation) {
