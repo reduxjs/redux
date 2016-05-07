0c088feaa57aa493cf652949214d29b1c13e727d
Julien Fontanet <julien.fontanet@isonoe.net>
2016-05-07 11:52:07 +0200

    feat(compose): optimize one function case

--- src/compose.js
+++ src/compose.js
@@ -10,11 +10,16 @@
  */
 
 export default function compose(...funcs) {
-  if (funcs.length === 0) {
+  const { length } = funcs
+  if (!length) {
     return arg => arg
-  } else {
-    const last = funcs[funcs.length - 1]
-    const rest = funcs.slice(0, -1)
-    return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args))
   }
+  
+  if (length === 1) {
+    return funcs[0]
+  }
+  
+  const last = funcs[funcs.length - 1]
+  const rest = funcs.slice(0, -1)
+  return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args))
 }
