050d42517330f9bbed73d37c13de84ea83a87230
Julien Fontanet <julien.fontanet@isonoe.net>
2016-05-08 13:45:00 +0200

    feat(compose): optimize one function case (#1701)
    * feat(compose): optimize one function case
    
    * style(compose): do not use a variable for funcs.length
    
    * test(compose): add test for a single param

--- src/compose.js
+++ src/compose.js
@@ -12,9 +12,13 @@
 export default function compose(...funcs) {
   if (funcs.length === 0) {
     return arg => arg
-  } else {
-    const last = funcs[funcs.length - 1]
-    const rest = funcs.slice(0, -1)
-    return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args))
   }
+
+  if (funcs.length === 1) {
+    return funcs[0]
+  }
+
+  const last = funcs[funcs.length - 1]
+  const rest = funcs.slice(0, -1)
+  return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args))
 }
