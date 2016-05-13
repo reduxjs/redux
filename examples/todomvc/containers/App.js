93d77bd5218620092dffbc01ef69f1a4dca40276
Graham Mendick <graham.mendick@gmail.com>
2016-05-13 14:00:55 +0100

    Added filter and stateNavigator props to App

--- examples/todomvc/containers/App.js
+++ examples/todomvc/containers/App.js
@@ -7,11 +7,14 @@ import * as TodoActions from '../actions'
 
 class App extends Component {
   render() {
-    const { todos, actions } = this.props
+    const { todos, actions, filter, stateNavigator } = this.props
     return (
       <div>
         <Header addTodo={actions.addTodo} />
-        <MainSection todos={todos} actions={actions} />
+        <MainSection todos={todos}
+                     actions={actions}
+                     filter={filter}
+                     stateNavigator={stateNavigator} />
       </div>
     )
   }
@@ -19,7 +22,9 @@ class App extends Component {
 
 App.propTypes = {
   todos: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  actions: PropTypes.object.isRequired,
+  filter: PropTypes.string.isRequired,
+  stateNavigator: PropTypes.object.isRequired
 }
 
 function mapStateToProps(state) {
