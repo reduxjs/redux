3eb466152c6f51b68690bbb90f5920098d72fd11
Graham Mendick <graham.mendick@gmail.com>
2016-05-13 13:56:13 +0100

    Rendered App inside navigated handler

--- examples/todomvc/index.js
+++ examples/todomvc/index.js
@@ -4,13 +4,19 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import configureStore from './store/configureStore'
+import configureRouter from './router/configureRouter'
 import 'todomvc-app-css/index.css'
 
 const store = configureStore()
+const stateNavigator = configureRouter()
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+stateNavigator.states.todomvc.navigated = function (data) {
+  render(
+    <Provider store={store}>
+      <App filter={data.data} stateNavigator={stateNavigator} />
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+stateNavigator.start()
