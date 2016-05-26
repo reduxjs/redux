49627c3a8b8fd8e57bf3f9009be1aeb84a514e18
Kerry Liu <kerryliu@gmail.com>
2016-05-26 13:42:44 -0700

    warn about unexpected shape once

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -113,14 +113,15 @@ export default function combineReducers(reducers) {
       throw sanityError
     }
 
+    var hasChanged = false
     if (process.env.NODE_ENV !== 'production') {
       var warningMessage = getUnexpectedStateShapeWarningMessage(state, finalReducers, action)
       if (warningMessage) {
         warning(warningMessage)
+        hasChanged = true
       }
     }
 
-    var hasChanged = false
     var nextState = {}
     for (var i = 0; i < finalReducerKeys.length; i++) {
       var key = finalReducerKeys[i]
@@ -134,7 +135,6 @@ export default function combineReducers(reducers) {
       nextState[key] = nextStateForKey
       hasChanged = hasChanged || nextStateForKey !== previousStateForKey
     }
-    hasChanged = hasChanged || Object.keys(state).length !== finalReducerKeys.length
     return hasChanged ? nextState : state
   }
 }
