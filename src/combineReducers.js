77cf9dcbaf1102ad31e1f2134fff48721a771fd5
Alan Jones <alan@thriveon.com>
2016-05-29 06:54:06 -0700

    Fixing JSDoc

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -107,7 +107,7 @@ function cleanReducerTree(rootReducerTree) {
  * into a single state object, whose keys correspond to the keys of the passed
  * reducer functions.
  *
- * @param {Object} reducers An object whose values correspond to different
+ * @param {Object} rootReducerTree An object whose values correspond to different
  * reducer functions that need to be combined into one. One handy way to obtain
  * it is to use ES6 `import * as reducers` syntax. The reducers may never return
  * undefined for any action. Instead, they should return their initial state
