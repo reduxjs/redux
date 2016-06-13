7f6446e3238984cba83096dc496e8c77409f32ab
Raine Revere <raineorshine@gmail.com>
2016-06-13 16:01:59 -0600

    preloadedState is optional

--- index.d.ts
+++ index.d.ts
@@ -195,7 +195,7 @@ export interface Store<S> {
  */
 export interface StoreCreator {
   <S>(reducer: Reducer<S>, enhancer?: StoreEnhancer<S>): Store<S>;
-  <S>(reducer: Reducer<S>, preloadedState: S, enhancer?: StoreEnhancer<S>): Store<S>;
+  <S>(reducer: Reducer<S>, preloadedState?: S, enhancer?: StoreEnhancer<S>): Store<S>;
 }
 
 /**
@@ -218,7 +218,7 @@ export interface StoreCreator {
  */
 export type StoreEnhancer<S> = (next: StoreEnhancerStoreCreator<S>) => StoreEnhancerStoreCreator<S>;
 export type GenericStoreEnhancer = <S>(next: StoreEnhancerStoreCreator<S>) => StoreEnhancerStoreCreator<S>;
-export type StoreEnhancerStoreCreator<S> = (reducer: Reducer<S>, preloadedState: S) => Store<S>;
+export type StoreEnhancerStoreCreator<S> = (reducer: Reducer<S>, preloadedState?: S) => Store<S>;
 
 /**
  * Creates a Redux store that holds the state tree.
