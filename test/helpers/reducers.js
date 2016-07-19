b368bd56cd3e20d47f376f9ccee33f08e87c063b
Mike Wilcox <mwilcox56@gmail.com>
2016-07-19 11:36:09 -0400

    throw if getState, subscribe, or unsubscribe called while dispatching (#1569)
    * throw error if getState, subscribe, or unsubscribe called while dispatching
    
    * prevent throwing if not subscribed
    
    * update getState error message
    
    * fix space after period
    
    * update subscribe/unsubscribe error messages

--- test/helpers/reducers.js
+++ test/helpers/reducers.js
@@ -1,4 +1,11 @@
-import { ADD_TODO, DISPATCH_IN_MIDDLE, THROW_ERROR } from './actionTypes'
+import { 
+  ADD_TODO, 
+  DISPATCH_IN_MIDDLE, 
+  GET_STATE_IN_MIDDLE, 
+  SUBSCRIBE_IN_MIDDLE,
+  UNSUBSCRIBE_IN_MIDDLE,
+  THROW_ERROR 
+} from './actionTypes'
 
 
 function id(state = []) {
@@ -40,6 +47,36 @@ export function dispatchInTheMiddleOfReducer(state = [], action) {
   switch (action.type) {
     case DISPATCH_IN_MIDDLE:
       action.boundDispatchFn()
+      return state
+    default:
+      return state
+  }
+}
+
+export function getStateInTheMiddleOfReducer(state = [], action) {
+  switch (action.type) {
+    case GET_STATE_IN_MIDDLE:
+      action.boundGetStateFn()
+      return state
+    default:
+      return state
+  }
+}
+
+export function subscribeInTheMiddleOfReducer(state = [], action) {
+  switch (action.type) {
+    case SUBSCRIBE_IN_MIDDLE:
+      action.boundSubscribeFn()
+      return state
+    default:
+      return state
+  }
+}
+
+export function unsubscribeInTheMiddleOfReducer(state = [], action) {
+  switch (action.type) {
+    case UNSUBSCRIBE_IN_MIDDLE:
+      action.boundUnsubscribeFn()
       return state
     default:
       return state
