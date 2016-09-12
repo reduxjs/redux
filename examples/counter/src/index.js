18d9cc170329dc1ea51b3d99cd35c19d7fcc0a59
ChrisJamesC <christopher.chiche@gmail.com>
2016-09-12 17:35:58 +0200

    Fat arrow functions in the counter example

--- examples/counter/src/index.js
+++ examples/counter/src/index.js
@@ -7,16 +7,14 @@ import counter from './reducers'
 const store = createStore(counter)
 const rootEl = document.getElementById('root')
 
-function render() {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-    />,
-    rootEl  
-  )
-}
+const render = () => ReactDOM.render(
+  <Counter
+    value={store.getState()}
+    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+  />,
+  rootEl
+)
 
 render()
 store.subscribe(render)
