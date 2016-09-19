51b106ccbab38b737b5b77613d72411f1756b2da
ChrisJamesC <christopher.chiche@gmail.com>
2016-09-19 10:25:43 +0200

    Style todos-with-undo examples

--- examples/todos-with-undo/src/containers/UndoRedo.js
+++ examples/todos-with-undo/src/containers/UndoRedo.js
@@ -13,19 +13,15 @@ let UndoRedo = ({ canUndo, canRedo, onUndo, onRedo }) => (
   </p>
 )
 
-const mapStateToProps = (state) => {
-  return {
-    canUndo: state.todos.past.length > 0,
-    canRedo: state.todos.future.length > 0
-  }
-}
+const mapStateToProps = (state) => ({
+  canUndo: state.todos.past.length > 0,
+  canRedo: state.todos.future.length > 0
+})
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onUndo: () => dispatch(UndoActionCreators.undo()),
-    onRedo: () => dispatch(UndoActionCreators.redo())
-  }
-}
+const mapDispatchToProps = ({
+  onUndo: UndoActionCreators.undo,
+  onRedo: UndoActionCreators.redo
+})
 
 UndoRedo = connect(
   mapStateToProps,
