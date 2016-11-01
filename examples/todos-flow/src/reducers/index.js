bc3f2aeb669f4ad5e424f6a711fd588e9bd3462e
Giulio Canti <giulio.canti@gmail.com>
2016-11-01 02:14:32 +0100

    update todos-flow example to latest versions: redux libdef (flow-typed) and Flow (v0.33) (#2051)
    * update redux libdef to lastest version
    
    * use combineReducer in todos-flow example

--- examples/todos-flow/src/reducers/index.js
+++ examples/todos-flow/src/reducers/index.js
@@ -1,12 +1,11 @@
 // @flow
+import { combineReducers } from 'redux'
 import todos from './todos'
 import visibilityFilter from './visibilityFilter'
-import type { State, Action } from '../types'
 
-export default function todoApp(state: ?State, action: Action): State {
-  const s = state || {}
-  return {
-    todos: todos(s.todos, action),
-    visibilityFilter: visibilityFilter(s.visibilityFilter, action)
-  }
-}
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+})
+
+export default todoApp
