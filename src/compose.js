44dfc39c3f8e5e8b51eeab7c44057da6c1086752
Joe Cortopassi <joe@joecortopassi.com>
2016-12-09 17:36:45 -0800

    Simplify composer

--- src/compose.js
+++ src/compose.js
@@ -20,7 +20,5 @@ export default function compose(...funcs) {
     return funcs[0]
   }
 
-  const last = funcs[funcs.length - 1]
-  const rest = funcs.slice(0, -1)
-  return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args))
+  return funcs.reduce((a, b) => (...args) => a(b(...args)))
 }
