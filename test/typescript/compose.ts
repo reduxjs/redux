5e8f3f8943dead1aaeed23118ecf2d525f32e583
Aleh Kashnikau <aleh.kashnikau@gmail.com>
2017-01-05 23:05:18 +0300

    Mapped type for combineReducers in index.d.ts (#2182)
    * Add mapped type for combineReducers in index.d.ts
    
    Updated typescript to 2.1.4
    Updated typescript-definition-tester to 0.0.5
    Updated typescript tests to use proper import
    Added mapped type to index.d.ts
    
    * add strict null check for reducer
    
    Updated Reducer<S> type in index.d.ts
    Add strictNullChecks flag to typescript spec

--- test/typescript/compose.ts
+++ test/typescript/compose.ts
@@ -1,4 +1,4 @@
-import {compose} from "../../index";
+import {compose} from "../../"
 
 // copied from DefinitelyTyped/compose-function
 
--- test/typescript/dispatch.ts
+++ test/typescript/dispatch.ts
@@ -1,4 +1,4 @@
-import {Dispatch, Action} from "../../index";
+import {Dispatch, Action} from "../../"
 
 
 declare const dispatch: Dispatch<any>;
@@ -6,7 +6,7 @@ declare const dispatch: Dispatch<any>;
 const dispatchResult: Action = dispatch({type: 'TYPE'});
 
 // thunk
-declare module "../../index" {
+declare module "../../" {
     export interface Dispatch<S> {
         <R>(asyncAction: (dispatch: Dispatch<S>, getState: () => S) => R): R;
     }
