4b370df30022141208db0a0d96ff8eec606a3dec
Anton Yefremov <anton.yefremov@gmail.com>
2017-02-02 16:15:11 +0200

    Remove `Symbol()` from real-world example

--- examples/real-world/src/middleware/api.js
+++ examples/real-world/src/middleware/api.js
@@ -73,7 +73,7 @@ export const Schemas = {
 }
 
 // Action key that carries API call info interpreted by this Redux middleware.
-export const CALL_API = Symbol('Call API')
+export const CALL_API = 'Call API'
 
 // A Redux middleware that interprets actions with CALL_API info specified.
 // Performs the call and promises when such actions are dispatched.
