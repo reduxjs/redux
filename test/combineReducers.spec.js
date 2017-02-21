611721fc75164d2ab8ccde6e8c8969e30d9fd36d
linzhi <526252549@qq.com>
2017-02-21 23:45:40 +0800

    use reducerKey to run reducer

--- test/combineReducers.spec.js
+++ test/combineReducers.spec.js
@@ -292,5 +292,19 @@ describe('Utils', () => {
       spy.mockClear()
       console.error = preSpy
     })
+
+    it('returns a composite reducer that maps the state keys to given reducers and use reducerKey to run reducer', () => {
+      const reducer = combineReducers({
+        counter: (state = 0, action) =>
+        action.type === 'increment' ? state + 1 : state,
+        stack: (state = [], action) =>
+        action.type === 'push' ? [ ...state, action.value ] : state
+      })
+
+      const s1 = reducer({}, { type: 'increment' })
+      expect(s1).toEqual({ counter: 1, stack: [] })
+      const s2 = reducer(s1, { reducerKey: 'stack', type: 'push', value: 'a' })
+      expect(s2).toEqual({ counter: 1, stack: [ 'a' ] })
+    })
   })
 })
