758393cc5617def4c9155d9494176ca040ec0063
flipactual <flipactual@gmail.com>
2017-03-02 21:57:33 -0800

    simplify compose to include only one special case

--- src/compose.js
+++ src/compose.js
@@ -8,15 +8,8 @@
  * from right to left. For example, compose(f, g, h) is identical to doing
  * (...args) => f(g(h(...args))).
  */
-
 export default function compose(...funcs) {
-  if (funcs.length === 0) {
-    return arg => arg
-  }
-
-  if (funcs.length === 1) {
-    return funcs[0]
-  }
-
-  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+  return funcs.length === 1
+    ? funcs[0]
+    : funcs.reduce((a, b) => (...args) => a(b(...args)), arg => arg)
 }
