f81e3aa272085a739dafb6b4d70071719438b636
Mateusz Burzyński <mateuszburzynski@gmail.com>
2017-03-19 19:57:58 +0100

    Fixed quote types inconsistency in a warning message

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -4,10 +4,10 @@ import warning from './utils/warning'
 
 function getUndefinedStateErrorMessage(key, action) {
   const actionType = action && action.type
-  const actionName = (actionType && `"${actionType.toString()}"`) || 'an action'
+  const actionDescription = (actionType && `action "${String(actionType)}"`) || 'an action'
 
   return (
-    `Given action ${actionName}, reducer "${key}" returned undefined. ` +
+    `Given ${actionDescription}, reducer "${key}" returned undefined. ` +
     `To ignore an action, you must explicitly return the previous state. ` +
     `If you want this reducer to hold no value, you can return null instead of undefined.`
   )
