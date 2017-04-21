35a4b48e2560e2c11b27cd18aac12167d0c832c4
Konstantinos Leimonis <k.leimonis@gmail.com>
2017-04-22 01:59:38 +0300

    Update real-world example with React v15.5.0

--- examples/real-world/src/components/Explore.js
+++ examples/real-world/src/components/Explore.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 const GITHUB_REPO = 'https://github.com/reactjs/redux'
 
--- examples/real-world/src/containers/UserPage.js
+++ examples/real-world/src/containers/UserPage.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { loadUser, loadStarred } from '../actions'
 import User from '../components/User'
