9a016e8719a7845d36a1531303acdaed0998be52
FUJI Goro (gfx) <gfuji@cpan.org>
2017-04-27 13:42:23 +0900

    add a hook to redux-devtools-extension
    https://github.com/zalmoxisus/redux-devtools-extension

--- examples/todos/src/index.js
+++ examples/todos/src/index.js
@@ -5,7 +5,10 @@ import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 render(
   <Provider store={store}>
