83f9b93aabe5bd2d8c87d2cfa53c04a442ee8bb7
Jeno Liu <Jeno.Liu@innovasolutions.local>
2017-05-16 16:46:37 +0800

    fix assertReducerShape

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -69,17 +69,6 @@ function assertReducerShape(reducers) {
       )
     }
 
-    const type = '@@redux/PROBE_UNKNOWN_ACTION_' + Math.random().toString(36).substring(7).split('').join('.')
-    if (typeof reducer(undefined, { type }) === 'undefined') {
-      throw new Error(
-        `Reducer "${key}" returned undefined when probed with a random type. ` +
-        `Don't try to handle ${ActionTypes.INIT} or other actions in "redux/*" ` +
-        `namespace. They are considered private. Instead, you must return the ` +
-        `current state for any unknown actions, unless it is undefined, ` +
-        `in which case you must return the initial state, regardless of the ` +
-        `action type. The initial state may not be undefined, but can be null.`
-      )
-    }
   })
 }
 
