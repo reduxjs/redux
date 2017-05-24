9388f258ba5b2daa38e1c984d091d3e2f81a688e
Aaron Reisman <aaron@lifeiscontent.net>
2017-05-24 17:07:46 +0700

    Remove snapshop for selector test

--- examples/todos-flow/src/__tests__/selectors/todos.js
+++ examples/todos-flow/src/__tests__/selectors/todos.js
@@ -7,24 +7,31 @@ describe('visibleTodosSelector', () => {
   beforeEach(() => {
     state = {
       todos: [
-        { id: 0, text: 'Test Todo', completed: false },
-        { id: 0, text: 'Test Todo', completed: false }
+        { id: 0, text: 'Test Todo 1', completed: false },
+        { id: 1, text: 'Test Todo 2', completed: true }
       ],
       visibilityFilter: 'SHOW_ALL'
     };
   });
 
   test('should handle SHOW_ALL', () => {
-    expect(visibleTodosSelector(state)).toMatchSnapshot();
+    expect(visibleTodosSelector(state)).toEqual([
+      { id: 0, text: 'Test Todo 1', completed: false },
+      { id: 1, text: 'Test Todo 2', completed: true }
+    ]);
   });
 
   test('should handle SHOW_ACTIVE', () => {
     state.visibilityFilter = 'SHOW_ACTIVE';
-    expect(visibleTodosSelector(state)).toMatchSnapshot();
+    expect(visibleTodosSelector(state)).toEqual([
+      { id: 0, text: 'Test Todo 1', completed: false }
+    ]);
   });
 
   test('should handle SHOW_COMPLETED', () => {
     state.visibilityFilter = 'SHOW_COMPLETED';
-    expect(visibleTodosSelector(state)).toMatchSnapshot();
+    expect(visibleTodosSelector(state)).toEqual([
+      { id: 1, text: 'Test Todo 2', completed: true }
+    ]);
   });
 });
