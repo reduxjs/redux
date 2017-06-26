cb57d01ad6e34328fee1a9c36dfe1981970fd943
Art Gillespie <art@Arts-MacBook-Pro-3452.local>
2017-06-25 19:37:19 -0700

    Revert "Add warning when bindActionCreators encounters non-function property (#2279)"
    This reverts commit 1b154e0ba79ff3428342618f5e437363f9e73569.

--- src/bindActionCreators.js
+++ src/bindActionCreators.js
@@ -1,5 +1,3 @@
-import warning from './utils/warning'
-
 function bindActionCreator(actionCreator, dispatch) {
   return (...args) => dispatch(actionCreator(...args))
 }
@@ -44,8 +42,6 @@ export default function bindActionCreators(actionCreators, dispatch) {
     const actionCreator = actionCreators[key]
     if (typeof actionCreator === 'function') {
       boundActionCreators[key] = bindActionCreator(actionCreator, dispatch)
-    } else {
-      warning(`bindActionCreators expected a function actionCreator for key '${key}', instead received type '${typeof actionCreator}'.`)
     }
   }
   return boundActionCreators
