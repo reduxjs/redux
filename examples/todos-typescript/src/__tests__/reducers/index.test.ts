82aa01cce9acd3c86b78c00892134c521f8d3f43
Juraj Husar <jurosh@gmail.com>
2017-08-24 15:40:19 +0200

    Update of typescript example to new redux redux

--- examples/todos-typescript/src/__tests__/reducers/index.test.ts
+++ examples/todos-typescript/src/__tests__/reducers/index.test.ts
@@ -1,8 +1,9 @@
 import reducer from '../../reducers';
+import { State } from '../../types';
 
 describe('root reducer', () => {
   it('should combine all reducers', () => {
-    expect(reducer({}, { type: '@@INIT' })).toEqual({
+    expect(reducer({} as State, { type: '@@INIT' })).toEqual({
       todos: [],
       visibilityFilter: 'SHOW_ALL'
     });
--- examples/todos-typescript/src/reducers/index.ts
+++ examples/todos-typescript/src/reducers/index.ts
@@ -1,14 +1,9 @@
 import { combineReducers } from 'redux';
-
+import { State } from '../types';
 import todos from './todos';
 import visibilityFilter from './visibilityFilter';
 
-// TODO: waiting for fix of Action typings.
-// Bug: We cannot use plain aciton like { type: 'DO' } but need to extend Action object.
-// Fix is already in index.d.ts@master just need to wait till new redux is released.
-const combine: any = combineReducers;
-
-export default combine({
+export default combineReducers<State>({
   todos,
   visibilityFilter
 });
