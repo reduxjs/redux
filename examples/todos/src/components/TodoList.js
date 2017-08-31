147ac20a813e58caad69276d2fc1c5c448fa6d62
Arthur Shpakov <arthur@amplience.com>
2017-08-31 21:52:41 +0300

    removed isRequired from todos, otherwise demo was broken

--- examples/todos/src/components/TodoList.js
+++ examples/todos/src/components/TodoList.js
@@ -19,7 +19,7 @@ TodoList.propTypes = {
     id: PropTypes.number.isRequired,
     completed: PropTypes.bool.isRequired,
     text: PropTypes.string.isRequired
-  }).isRequired).isRequired,
+  }).isRequired),
   onTodoClick: PropTypes.func.isRequired
 }
 
