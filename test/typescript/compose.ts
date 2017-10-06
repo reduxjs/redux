7bea62ad401046294b4bde4db09b7e2378192ef7
Tom Crockett <pelotom@gmail.com>
2017-10-05 18:04:20 -0700

    Revamp TypeScript typing with more type safety (#2563)
    * Revamp TypeScript typing with more type safety
    
    * Provide a default action type for combineReducers
    
    * Change state default types to any
    
    * Don't parameterize Dispatch with a state type
    
    * Remove docstring about excised type parameter

--- test/typescript/compose.ts
+++ test/typescript/compose.ts
@@ -36,4 +36,4 @@ const t11: number = compose(stringToNumber, numberToString, stringToNumber,
 
 
 const funcs = [stringToNumber, numberToString, stringToNumber];
-const t12 = compose(...funcs)('bar', 42, true);
+const t12 = compose(...funcs)('bar');
