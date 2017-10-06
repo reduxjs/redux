628928e3108df9725f07689e3785b5a2a226baa8
Parakleta <parakleta@darkreality.org>
2017-10-06 12:22:50 +1100

    Make bindActionCreators transparently pass `this`. (#2641)

--- test/bindActionCreators.spec.js
+++ test/bindActionCreators.spec.js
@@ -33,6 +33,21 @@ describe('bindActionCreators', () => {
     ])
   })
 
+  it('wraps action creators transparently', () => {
+    const uniqueThis = {}
+    const argArray = [1, 2, 3]
+    function actionCreator() {
+      return { type: 'UNKNOWN_ACTION', this: this, args: [...arguments] }
+    }
+    const boundActionCreator = bindActionCreators(actionCreator, store.dispatch)
+
+    const boundAction = boundActionCreator.apply(uniqueThis,argArray)
+    const action = actionCreator.apply(uniqueThis,argArray)
+    expect(boundAction).toEqual(action)
+    expect(boundAction.this).toBe(uniqueThis)
+    expect(action.this).toBe(uniqueThis)
+  })
+
   it('skips non-function values in the passed object', () => {
     const boundActionCreators = bindActionCreators({
       ...actionCreators,
