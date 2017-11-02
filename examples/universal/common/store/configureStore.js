52b4ede4749edecd2b3932f7fad5ae7df1e7ed1a
Steve Mao <maochenyan@gmail.com>
2017-11-02 11:50:49 +1100

    no need to require `rootReducer` again
    [`babel-preset-react-app` does not transform modules to CJS](https://github.com/facebookincubator/create-react-app/blob/master/packages/babel-preset-react-app/index.js#L107-L108) so I don't think we need to require `rootReducer` again

--- examples/universal/common/store/configureStore.js
+++ examples/universal/common/store/configureStore.js
@@ -12,8 +12,7 @@ const configureStore = (preloadedState) => {
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers').default
-      store.replaceReducer(nextRootReducer)
+      store.replaceReducer(rootReducer)
     })
   }
 
