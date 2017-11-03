2b90f97e0b2f145058393d6153281d64e9acff37
Tim Dorr <timdorr@users.noreply.github.com>
2017-11-03 13:24:19 -0400

    REPLACE action for replaceReducers (#2673)
    * Add an explicit private action type for replacing reducers
    
    * Make this a const while we're at it...
    
    * Triple equals for lint
    
    * Add a random string to the "private" actions.

--- src/utils/actionTypes.js
+++ src/utils/actionTypes.js
@@ -4,8 +4,9 @@
  * If the current state is undefined, you must return the initial state.
  * Do not reference these action types directly in your code.
  */
-var ActionTypes = {
-  INIT: '@@redux/INIT'
+const ActionTypes = {
+  INIT: '@@redux/INIT' + Math.random().toString(36).substring(7).split('').join('.'),
+  REPLACE: '@@redux/REPLACE' + Math.random().toString(36).substring(7).split('').join('.')
 }
 
 export default ActionTypes
