2d415df4c52f4ec46ad2416cdeb80789bba3e57c
Dmitriy Serdtsev <in19farkt@gmail.com>
2017-11-07 15:35:22 +0700

    Updated typescript to 2.1v. Improved type for combineReducers function.

--- test/typescript/compose.ts
+++ test/typescript/compose.ts
@@ -1,39 +1,47 @@
 import {compose} from "../../index";
 
-// copied from DefinitelyTyped/compose-function
+type First = 'first';
+type Second = 'second';
+type Third = 'third';
+type Fourth = 'fourth';
 
-const numberToNumber = (a: number): number => a + 2;
-const numberToString = (a: number): string => "foo";
-const stringToNumber = (a: string): number => 5;
+const first: First = 'first';
+const second: Second = 'second';
+const third: Third = 'third';
+const fourth: Fourth = 'fourth';
 
-const t1: number = compose(numberToNumber, numberToNumber)(5);
-const t2: string = compose(numberToString, numberToNumber)(5);
-const t3: string = compose(numberToString, stringToNumber)("f");
-const t4: (a: string) => number = compose(
-  (f: (a: string) => number) => ((p: string) => 5),
-  (f: (a: number) => string) => ((p: string) => 4)
-)(numberToString);
+const firstToSecond = (a: First): Second => 'second';
+const secondToThird = (a: Second): Third => 'third';
+const thirdToFourth = (a: Third): Fourth => 'fourth';
 
+const funcWithZeroArg = (): First => 'first';
+const funcWithOneArg = (a: number): First => 'first';
+const funcWithTwoArgs = (a: number, b: boolean): First => 'first';
+const funcWithThreeArgs = (a: number, b: boolean, c: string): First => 'first';
 
-const t5: number = compose(stringToNumber, numberToString, numberToNumber)(5);
-const t6: string = compose(numberToString, stringToNumber, numberToString,
-  numberToNumber)(5);
+// no args
+const r1: First = compose()('first');
 
-const t7: string = compose(
-  numberToString, numberToNumber, stringToNumber, numberToString,
-  stringToNumber)("fo");
+// ane functions
+const r2: Second = compose(firstToSecond)('first');
 
+// two functions
+const r3: Second = compose(firstToSecond, funcWithZeroArg)();
+const r4: Second = compose(firstToSecond, funcWithOneArg)(5);
+const r5: Second = compose(firstToSecond, funcWithTwoArgs)(5, false);
+const r6: Second = compose(firstToSecond, funcWithThreeArgs)(5, false, 'string');
 
-const multiArgFn = (a: string, b: number, c: boolean): string => 'foo'
+// three functions
+const r7: Third = compose(secondToThird, firstToSecond, funcWithZeroArg)();
+const r8: Third = compose(secondToThird, firstToSecond, funcWithOneArg)(5);
+const r9: Third = compose(secondToThird, firstToSecond, funcWithTwoArgs)(5, false);
+const r10: Third = compose(secondToThird, firstToSecond, funcWithThreeArgs)(5, false, 'string');
 
-const t8: string = compose(multiArgFn)('bar', 42, true);
-const t9: number = compose(stringToNumber, multiArgFn)('bar', 42, true);
-const t10: string = compose(numberToString, stringToNumber,
-  multiArgFn)('bar', 42, true);
+// four functions
+const r11: Fourth = compose(thirdToFourth, secondToThird, firstToSecond, funcWithZeroArg)();
+const r12: Fourth = compose(thirdToFourth, secondToThird, firstToSecond, funcWithOneArg)(5);
+const r13: Fourth = compose(thirdToFourth, secondToThird, firstToSecond, funcWithTwoArgs)(5, false);
+const r14: Fourth = compose(thirdToFourth, secondToThird, firstToSecond, funcWithThreeArgs)(5, false, 'string');
 
-const t11: number = compose(stringToNumber, numberToString, stringToNumber,
-  multiArgFn)('bar', 42, true);
-
-
-const funcs = [stringToNumber, numberToString, stringToNumber];
-const t12 = compose(...funcs)('bar', 42, true);
+// more than four functions
+const r15: Fourth = compose(thirdToFourth, secondToThird, firstToSecond, funcWithZeroArg, funcWithZeroArg)('first');
