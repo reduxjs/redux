195458d6d1129360682849af8e6ee1934306f293
Pete Schuster <pas5027@gmail.com>
2017-11-20 21:02:15 -0500

    adds shared route component

--- examples/real-world/src/containers/Root.prod.js
+++ examples/real-world/src/containers/Root.prod.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
-import { Route } from 'react-router-dom'
-import App from './App'
-import UserPage from './UserPage'
-import RepoPage from './RepoPage'
+import Routes from '../Routes'
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <Route path="/" component={App} />
-      <Route path="/:login/:name"
-             component={RepoPage} />
-      <Route path="/:login"
-             component={UserPage} />
+    <Routes />
   </Provider>
 )
 
