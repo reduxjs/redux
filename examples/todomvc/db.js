70952ed86f2d0520a67f043894644cb662a4dd30
tb <t.bak@selleo.com>
2018-01-22 23:00:44 +0100

    Add server

---
+++ examples/todomvc/db.js
@@ -0,0 +1,19 @@
+module.exports = () => ({
+  todos: [
+    {
+      completed: true,
+      id: 1,
+      text: 'Json-mock'
+    },
+    {
+      completed: true,
+      id: 2,
+      text: 'React'
+    },
+    {
+      completed: false,
+      id: 3,
+      text: 'Redux'
+    }
+  ]
+})
