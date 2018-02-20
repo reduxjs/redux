51539fae61cfb2ef6abfab08065bfd46a5779f8a
Matias H. Leidemer <matiasleidemer@gmail.com>
2018-02-20 10:37:56 -0300

    Fix examples and todo app with VisibilityFilters export (#2848)
    Kudos to @antoniwan for spotting the issue.

--- examples/todos/src/actions/index.js
+++ examples/todos/src/actions/index.js
@@ -14,3 +14,9 @@ export const toggleTodo = (id) => ({
   type: 'TOGGLE_TODO',
   id
 })
+
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
