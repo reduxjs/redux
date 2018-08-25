dd4c442f9b5ce711c5b183b68d4fa4bd6a8420cb
Victor Zagorodny <post.vittorius@gmail.com>
2018-08-26 00:18:33 +0300

    style: wrap too long lines in some specs

--- test/compose.spec.js
+++ test/compose.spec.js
@@ -6,8 +6,19 @@ describe('Utils', () => {
       const double = x => x * 2
       const square = x => x * x
       expect(compose(square)(5)).toBe(25)
-      expect(compose(square, double)(5)).toBe(100)
-      expect(compose(double, square, double)(5)).toBe(200)
+      expect(
+        compose(
+          square,
+          double
+        )(5)
+      ).toBe(100)
+      expect(
+        compose(
+          double,
+          square,
+          double
+        )(5)
+      ).toBe(200)
     })
 
     it('composes functions from right to left', () => {
@@ -16,26 +27,79 @@ describe('Utils', () => {
       const c = next => x => next(x + 'c')
       const final = x => x
 
-      expect(compose(a, b, c)(final)('')).toBe('abc')
-      expect(compose(b, c, a)(final)('')).toBe('bca')
-      expect(compose(c, a, b)(final)('')).toBe('cab')
+      expect(
+        compose(
+          a,
+          b,
+          c
+        )(final)('')
+      ).toBe('abc')
+      expect(
+        compose(
+          b,
+          c,
+          a
+        )(final)('')
+      ).toBe('bca')
+      expect(
+        compose(
+          c,
+          a,
+          b
+        )(final)('')
+      ).toBe('cab')
     })
 
     it('throws at runtime if argument is not a function', () => {
       const square = x => x * x
       const add = (x, y) => x + y
 
-      expect(() => compose(square, add, false)(1, 2)).toThrow()
-      expect(() => compose(square, add, undefined)(1, 2)).toThrow()
-      expect(() => compose(square, add, true)(1, 2)).toThrow()
-      expect(() => compose(square, add, NaN)(1, 2)).toThrow()
-      expect(() => compose(square, add, '42')(1, 2)).toThrow()
+      expect(() =>
+        compose(
+          square,
+          add,
+          false
+        )(1, 2)
+      ).toThrow()
+      expect(() =>
+        compose(
+          square,
+          add,
+          undefined
+        )(1, 2)
+      ).toThrow()
+      expect(() =>
+        compose(
+          square,
+          add,
+          true
+        )(1, 2)
+      ).toThrow()
+      expect(() =>
+        compose(
+          square,
+          add,
+          NaN
+        )(1, 2)
+      ).toThrow()
+      expect(() =>
+        compose(
+          square,
+          add,
+          '42'
+        )(1, 2)
+      ).toThrow()
     })
 
     it('can be seeded with multiple arguments', () => {
       const square = x => x * x
       const add = (x, y) => x + y
-      expect(compose(square, add)(1, 2)).toBe(9)
+      expect(
+        compose(
+          square,
+          add
+        )(1, 2)
+      ).toBe(9)
     })
 
     it('returns the first given argument if given no functions', () => {
