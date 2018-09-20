648d06d8705f7c261f084f7677e30c80370046fe
Josh Alling <joshralling@gmail.com>
2018-09-20 08:44:11 -0600

    move Header component out of Header container file (#3142)

---
+++ examples/todomvc/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import TodoTextInput from './TodoTextInput'
+
+const Header = ({ addTodo }) => (
+  <header className="header">
+    <h1>todos</h1>
+    <TodoTextInput
+      newTodo
+      onSave={(text) => {
+        if (text.length !== 0) {
+          addTodo(text)
+        }
+      }}
+      placeholder="What needs to be done?"
+    />
+  </header>
+)
+
+Header.propTypes = {
+  addTodo: PropTypes.func.isRequired
+}
+
+export default Header
