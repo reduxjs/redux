965f18bcb80f3505b171880473d4a17bfe88b9f0
gengjiawen <technicalcute@gmail.com>
2018-09-22 14:48:45 +0800

    use prettier format ts too

--- test/typescript/actionCreators.ts
+++ test/typescript/actionCreators.ts
@@ -1,11 +1,13 @@
 import {
-  ActionCreator, Action, Dispatch,
-  bindActionCreators, ActionCreatorsMapObject
-} from "redux"
-
+  ActionCreator,
+  Action,
+  Dispatch,
+  bindActionCreators,
+  ActionCreatorsMapObject
+} from 'redux'
 
 interface AddTodoAction extends Action {
-  text: string;
+  text: string
 }
 
 const addTodo: ActionCreator<AddTodoAction> = (text: string) => ({
@@ -13,37 +15,37 @@ const addTodo: ActionCreator<AddTodoAction> = (text: string) => ({
   text
 })
 
-const addTodoAction: AddTodoAction = addTodo('test');
+const addTodoAction: AddTodoAction = addTodo('test')
 
-type AddTodoThunk = (dispatch: Dispatch) => AddTodoAction;
+type AddTodoThunk = (dispatch: Dispatch) => AddTodoAction
 
-const addTodoViaThunk: ActionCreator<AddTodoThunk> = (text: string) =>
-  (dispatch: Dispatch) => ({
-    type: 'ADD_TODO',
-    text
-  })
+const addTodoViaThunk: ActionCreator<AddTodoThunk> = (text: string) => (
+  dispatch: Dispatch
+) => ({
+  type: 'ADD_TODO',
+  text
+})
 
-declare const dispatch: Dispatch;
+declare const dispatch: Dispatch
 
-const boundAddTodo = bindActionCreators(addTodo, dispatch);
+const boundAddTodo = bindActionCreators(addTodo, dispatch)
 
-const dispatchedAddTodoAction: AddTodoAction = boundAddTodo('test');
-
+const dispatchedAddTodoAction: AddTodoAction = boundAddTodo('test')
 
 const boundAddTodoViaThunk = bindActionCreators<
   ActionCreator<AddTodoThunk>,
   ActionCreator<AddTodoAction>
 >(addTodoViaThunk, dispatch)
 
-const dispatchedAddTodoViaThunkAction: AddTodoAction =
-  boundAddTodoViaThunk('test');
+const dispatchedAddTodoViaThunkAction: AddTodoAction = boundAddTodoViaThunk(
+  'test'
+)
 
+const boundActionCreators = bindActionCreators({ addTodo }, dispatch)
 
-const boundActionCreators = bindActionCreators({addTodo}, dispatch);
-
-const otherDispatchedAddTodoAction: AddTodoAction =
-  boundActionCreators.addTodo('test');
-
+const otherDispatchedAddTodoAction: AddTodoAction = boundActionCreators.addTodo(
+  'test'
+)
 
 interface M extends ActionCreatorsMapObject {
   addTodoViaThunk: ActionCreator<AddTodoThunk>
@@ -53,9 +55,13 @@ interface N extends ActionCreatorsMapObject {
   addTodoViaThunk: ActionCreator<AddTodoAction>
 }
 
-const boundActionCreators2 = bindActionCreators<M, N>({
-  addTodoViaThunk
-}, dispatch)
+const boundActionCreators2 = bindActionCreators<M, N>(
+  {
+    addTodoViaThunk
+  },
+  dispatch
+)
 
-const otherDispatchedAddTodoAction2: AddTodoAction =
-  boundActionCreators2.addTodoViaThunk('test');
+const otherDispatchedAddTodoAction2: AddTodoAction = boundActionCreators2.addTodoViaThunk(
+  'test'
+)
