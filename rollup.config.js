3acb00775178b25e4480847544e8f3d10467c60f
Tim Dorr <git@timdorr.com>
2018-09-25 14:09:51 -0400

    Simplify the Rollup build to one command.

--- rollup.config.js
+++ rollup.config.js
@@ -3,49 +3,78 @@ import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 import { terser } from 'rollup-plugin-terser'
 
-const env = process.env.NODE_ENV
-const config = {
-  input: 'src/index.js',
-  plugins: []
-}
-
-if (env === 'es' || env === 'cjs') {
-  config.output = { format: env, indent: false }
-  config.external = ['symbol-observable']
-  config.plugins.push(
-    babel({
-      plugins: ['external-helpers'],
-    })
-  )
-}
-
-if (env === 'development' || env === 'production') {
-  config.output = { format: 'umd', name: 'Redux', indent: false }
-  config.plugins.push(
-    nodeResolve({
-      jsnext: true
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      plugins: ['external-helpers'],
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(env)
-    })
-  )
-}
-
-if (env === 'production') {
-  config.plugins.push(
-    terser({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false
-      }
-    })
-  )
-}
-
-export default config
+export default [
+  {
+    input: 'src/index.js',
+    output: { file: 'lib/redux.js', format: 'cjs', indent: false },
+    external: ['symbol-observable'],
+    plugins: [
+      babel({
+        plugins: ['external-helpers']
+      })
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: { file: 'es/redux.js', format: 'es', indent: false },
+    external: ['symbol-observable'],
+    plugins: [
+      babel({
+        env: 'es',
+        plugins: ['external-helpers']
+      })
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/redux.js',
+      format: 'umd',
+      name: 'Redux',
+      indent: false
+    },
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      babel({
+        env: 'es',
+        exclude: 'node_modules/**',
+        plugins: ['external-helpers']
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('development')
+      })
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/redux.min.js',
+      format: 'umd',
+      name: 'Redux',
+      indent: false
+    },
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      babel({
+        env: 'es',
+        exclude: 'node_modules/**',
+        plugins: ['external-helpers']
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false
+        }
+      })
+    ]
+  }
+]
