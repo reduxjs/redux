6c568a7257880722bb0bb921c7445cf806d9ca07
Svein Petter Gjøby <sveinpg@users.noreply.github.com>
2018-10-08 16:13:36 +0200

    Updated copyright

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -57,7 +57,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: 'Copyright (c) 2015-present Dan Abramov.',
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
