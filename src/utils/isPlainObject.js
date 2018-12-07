b5602a2c4679f0bd30d8f3c78ae80ed5b036525f
Michaelangelo Jong <mike96jong@gmail.com>
2018-12-07 14:07:45 -0400

    Simplify isPlainObject better performance
    There is no need to go through the entire prototypal chain to see if `obj`'s prototype is equal to `Object.prototype`.

--- src/utils/isPlainObject.js
+++ src/utils/isPlainObject.js
@@ -3,12 +3,5 @@
  * @returns {boolean} True if the argument appears to be a plain object.
  */
 export default function isPlainObject(obj) {
-  if (typeof obj !== 'object' || obj === null) return false
-
-  let proto = obj
-  while (Object.getPrototypeOf(proto) !== null) {
-    proto = Object.getPrototypeOf(proto)
-  }
-
-  return Object.getPrototypeOf(obj) === proto
+  return obj != null && Object.getPrototypeOf(obj) === Object.prototype
 }
