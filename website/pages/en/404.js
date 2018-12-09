a3162f1d32baf48e562940b4afbfe83a42883652
sveinpg <sveinpg.gmail.com>
2018-12-09 22:57:14 +0100

    Add GA script for tracking 404s

--- website/pages/en/404.js
+++ website/pages/en/404.js
@@ -1,9 +1,29 @@
 const React = require("react");
+const siteConfig = require(`${process.cwd()}/siteConfig.js`);
 
 class ErrorPage extends React.Component {
+  getTrackingScript() {
+    if (!siteConfig.gaTrackingId) {
+      return null;
+    }
+
+    return {__html:`
+      ga('create', "${siteConfig.gaTrackingId}");
+      ga('send', {
+        hitType: 'event',
+        eventCategory: '404 Response',
+        eventAction: window.location.href,
+        eventLabel: document.referrer
+      });`
+    }
+  }
+
   render() {
+    const trackingScript = this.getTrackingScript();
+
     return (
       <div className="error-page">
+      {trackingScript && <script dangerouslySetInnerHTML={trackingScript}/>}
         <div className="error-message">
           <div className=" error-message-container container">
             <span>404 </span>
