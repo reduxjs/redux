cce5fca6cee4623a917abd1561808f97a310a982
Ilya Fadeev <fadeev.ilya@gmail.com>
2018-12-30 00:43:54 -0700

    compose: use identity function as a default argument for reduce

--- src/compose.js
+++ src/compose.js
@@ -10,13 +10,9 @@
  */
 
 export default function compose(...funcs) {
-  if (funcs.length === 0) {
-    return arg => arg
-  }
-
   if (funcs.length === 1) {
     return funcs[0]
   }
 
-  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+  return funcs.reduce((a, b) => (...args) => a(b(...args)), x => x)
 }
