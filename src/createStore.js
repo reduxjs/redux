1865990720f25cbb4350f6b323fdc8d302267379
Matthew Gerstman <matthew.gerstman@gmail.com>
2019-01-21 11:21:16 -0500

    Add a comment explaining REPLACE action

--- src/createStore.js
+++ src/createStore.js
@@ -231,6 +231,11 @@ export default function createStore(reducer, preloadedState, enhancer) {
     }
 
     currentReducer = nextReducer
+
+    // This action has a similiar effect to ActionTypes.INIT.
+    // Any reducers that existed in both the new and old rootReducer
+    // will receive the previous state. This effectively populates
+    // the new state tree with any relevant data from the old one.
     dispatch({ type: ActionTypes.REPLACE })
   }
 
