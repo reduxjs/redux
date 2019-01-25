ed74b7e14698106386f7009aa559c0d3c42e80fe
ecity_wangxiangjiu <920350341@qq.com>
2019-01-25 14:12:14 +0800

    test

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -1,7 +1,7 @@
 import ActionTypes from './utils/actionTypes'
 import warning from './utils/warning'
 import isPlainObject from './utils/isPlainObject'
-
+// test
 function getUndefinedStateErrorMessage(key, action) {
   const actionType = action && action.type
   const actionDescription =
