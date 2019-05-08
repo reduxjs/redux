782ca3197debdaea6ae82b4716a81dee8e70e15b
naor2205@gmail.com <naor2205@gmail.com>
2019-05-08 16:20:11 +0300

    remove semis

--- examples/todos-with-undo/src/containers/UndoRedo.js
+++ examples/todos-with-undo/src/containers/UndoRedo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ActionCreators as UndoActionCreators } from 'redux-undo';
+import { ActionCreators as UndoActionCreators } from 'redux-undo'
 import { connect } from 'react-redux'
 
 let UndoRedo = ({ canUndo, canRedo, onUndo, onRedo }) => (
