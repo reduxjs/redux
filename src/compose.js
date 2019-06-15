73fc93b625a3f000c7a6717163711f599b686059
Yang Lin <yanglin5689446@gmail.com>
2019-06-15 19:59:10 +0800

    Update implementation of compose

--- src/compose.js
+++ src/compose.js
@@ -10,13 +10,5 @@
  */
 
 export default function compose(...funcs) {
-  if (funcs.length === 0) {
-    return arg => arg
-  }
-
-  if (funcs.length === 1) {
-    return funcs[0]
-  }
-
-  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+  return funcs.reduce((a, b) => (...args) => a(b(...args)), arg => arg)
 }
