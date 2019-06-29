2ecdda175aeeda0d58505ea43114dcfe8f062428
junshun.mq <junshun.mq@alibaba-inc.com>
2019-06-29 16:27:24 +0800

    Modify: optimize compose，support more args

--- src/compose.js
+++ src/compose.js
@@ -18,5 +18,5 @@ export default function compose(...funcs) {
     return funcs[0]
   }
 
-  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+  return funcs.reduce((a, b) => (next, ...rest) => a(b(next, ...rest), ...rest))
 }
