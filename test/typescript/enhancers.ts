1ec118848ca77b9b5694ab703270675fd8347d1c
Chris Ackerman <chris@topher.land>
2019-08-02 08:52:25 -0700

    Made prettier happy.

--- test/typescript/enhancers.ts
+++ test/typescript/enhancers.ts
@@ -1,11 +1,5 @@
 import { PreloadedState } from '../../index'
-import {
-  StoreEnhancer,
-  Action,
-  AnyAction,
-  Reducer,
-  createStore
-} from 'redux'
+import { StoreEnhancer, Action, AnyAction, Reducer, createStore } from 'redux'
 
 interface State {
   someField: 'string'
