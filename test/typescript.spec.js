f5fd57e809588331ad2041e81c11bbedb555b429
Alex Krolick <alexander.krolick@gmail.com>
2019-08-11 16:19:34 -0700

    chore(typescript): remove typings
    BREAKING: remove TypeScript definitions from repo

--- test/typescript.spec.js
+++
@@ -1,7 +0,0 @@
-import { checkDirectory } from 'typings-tester'
-
-describe('TypeScript definitions', function() {
-  it('should compile against index.d.ts', () => {
-    checkDirectory(__dirname + '/typescript')
-  })
-})
