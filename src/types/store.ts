52554e543e6d24b8ab83cc191d231d200e5c11f4
Gregory Beaver <greg.beaver.cello@gmail.com>
2019-08-30 12:03:15 -0400

    extract middleware types from index.d.ts

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,7 +1,32 @@
 /// <reference types="symbol-observable" />
-import { Dispatch, PreloadedState } from '../..'
+import { PreloadedState } from '../..'
 import { Action, AnyAction } from './actions'
 import { Reducer } from './reducers'
+
+/**
+ * A *dispatching function* (or simply *dispatch function*) is a function that
+ * accepts an action or an async action; it then may or may not dispatch one
+ * or more actions to the store.
+ *
+ * We must distinguish between dispatching functions in general and the base
+ * `dispatch` function provided by the store instance without any middleware.
+ *
+ * The base dispatch function *always* synchronously sends an action to the
+ * store's reducer, along with the previous state returned by the store, to
+ * calculate a new state. It expects actions to be plain objects ready to be
+ * consumed by the reducer.
+ *
+ * Middleware wraps the base dispatch function. It allows the dispatch
+ * function to handle async actions in addition to actions. Middleware may
+ * transform, delay, ignore, or otherwise interpret actions or async actions
+ * before passing them to the next middleware.
+ *
+ * @template A The type of things (actions or otherwise) which may be
+ *   dispatched.
+ */
+export interface Dispatch<A extends Action = AnyAction> {
+  <T extends A>(action: T, ...extraArgs: any[]): T
+}
 
 /**
  * Function to remove listener added by `Store.subscribe()`.
