4cb4a5e4ae047307e4a79aa83a6fdf9df1bb246c
Tim Dorr <git@timdorr.com>
2019-09-06 20:55:56 -0400

    Remove the example content and tools.

--- examples/README.md
+++
@@ -1,3 +0,0 @@
-# Examples
-
-Read the descriptions for every example on the [Examples](../docs/introduction/Examples.md) documentation page.
--- examples/todos/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import todos from './todos'
-import visibilityFilter from './visibilityFilter'
-
-export default combineReducers({
-  todos,
-  visibilityFilter
-})
