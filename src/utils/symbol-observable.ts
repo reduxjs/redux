b2ac1b438eb5800f4d4649bab12ece1f42e32588
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-11-24 10:05:37 +0100

    Add PURE annotations

--- src/utils/symbol-observable.ts
+++ src/utils/symbol-observable.ts
@@ -1,2 +1,5 @@
-export default (() =>
-  (typeof Symbol === 'function' && Symbol.observable) || '@@observable')()
+const $$observable = /* #__PURE__ */ (() =>
+  (typeof Symbol === 'function' && (Symbol as any).observable) ||
+  '@@observable')()
+
+export default $$observable
