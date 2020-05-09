916626baa1a9766ccc297bab7f1df5608e88bc42
Nathan Bierema <nbierema@gmail.com>
2020-05-09 13:51:14 -0400

    Format

--- test/typescript.spec.ts
+++ test/typescript.spec.ts
@@ -1,6 +1,6 @@
 import { checkDirectory } from 'typings-tester'
 
-describe('TypeScript definitions', function() {
+describe('TypeScript definitions', function () {
   it('should compile against index.d.ts', () => {
     checkDirectory(__dirname + '/typescript')
   })
