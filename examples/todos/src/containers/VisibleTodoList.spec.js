c98cfbd7e2712ca394f852985d32637aee5b0fa6
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-06-14 09:48:09 -0700

    adds unit test boilerplate

--- examples/todos/src/containers/VisibleTodoList.spec.js
+++ examples/todos/src/containers/VisibleTodoList.spec.js
@@ -1,18 +1,33 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import rootReducer from '../reducers'
 import VisibleTodoList from './VisibleTodoList';
+import TodoList from '../Components/TodoList';
 
 const store = createStore(rootReducer)
 
-it('renders an empty list', () => {
-    const wrapper = mount(
-        <Provider store={store}>
-            <VisibleTodoList />
-        </Provider>
-    )
-    expect(wrapper.html()).toBe('<ul></ul>')
+describe("integration test with mount", () => {
+    it('renders an empty list', () => {
+        const wrapper = mount(
+            <Provider store={store}>
+                <VisibleTodoList />
+            </Provider>
+        )
+        expect(wrapper.html()).toBe('<ul></ul>')
+    })
+})
+
+describe("unit tests w/ shallow", () => {
+    it('renders an empty list', () => {
+        const wrapper = shallow(
+            <TodoList todos={[]} toggleTodo={jest.fn()} />
+        )
+        expect(wrapper.html()).toBe('<ul></ul>')
+    })
+})
+
+describe("unit tests w/ mount", () => {
 })
