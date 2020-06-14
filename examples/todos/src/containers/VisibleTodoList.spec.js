40625a6c5910b9e5388bec3e9ad0cc8a0d385391
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-06-14 10:46:00 -0700

    adds comments

--- examples/todos/src/containers/VisibleTodoList.spec.js
+++ examples/todos/src/containers/VisibleTodoList.spec.js
@@ -9,7 +9,9 @@ import TodoList from '../Components/TodoList';
 import { VisibilityFilters } from '../actions'
 
 
-
+// Feel free to mock out the <TodoList /> here to make it an integration test of only
+// the Container + Redux sub-components. This could still be considered an "integration test",
+// but it could integration test smaller or larger slices of functionality depending on your needs.
 describe("integration test, high verification", () => {
     it('renders an empty list', () => {
         const store = createStore(rootReducer)
@@ -65,6 +67,7 @@ describe("unit tests, high isolation", () => {
 
     describe("<VisibleTodoList /> container", () => {
         it('supplies an empty list', () => {
+            // Feel free to mock out TodoList here to increase the isolation of the container
             const store = createStore(rootReducer)
             const wrapper = mount(
                 <Provider store={store}>
@@ -75,6 +78,7 @@ describe("unit tests, high isolation", () => {
         })
 
         it('supplies a toggleTodo prop that toggles a todo', () => {
+            // Feel free to mock out TodoList here to increase the isolation of the container
             const store = createStore(rootReducer, {
                 todos: [
                     { id: 1, completed: false, text: 'hello world' }
