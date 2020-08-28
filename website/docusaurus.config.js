40888c07951117f24913d741d40252d2cc30ed1b
38elements <mh19820223@gmail.com>
2020-08-28 23:44:02 +0900

    Change image

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -7,7 +7,7 @@ module.exports = {
   organizationName: 'reduxjs',
   projectName: 'redux',
   themeConfig: {
-    image: 'https://redux.js.org/img/logo.png',
+    image: 'https://redux.js.org/img/redux-logo-landscape.png',
     disableDarkMode: false,
     prism: {
       theme: require('./src/js/monokaiTheme.js')
