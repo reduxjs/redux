2d98de5599b2d6150615f96a35038817208c5b7a
38elements <38elements@users.noreply.github.com>
2020-08-29 01:03:52 +0900

    Add og:image and twitter:image (#3870)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -7,6 +7,7 @@ module.exports = {
   organizationName: 'reduxjs',
   projectName: 'redux',
   themeConfig: {
+    image: 'https://redux.js.org/img/redux-logo-landscape.png',
     disableDarkMode: false,
     prism: {
       theme: require('./src/js/monokaiTheme.js')
