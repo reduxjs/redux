7a11fb6c992bef6dcf2243dc71e277f14988776e
Giacomo Cerquone <cerquone96@hotmail.it>
2020-10-25 17:53:27 +0100

    chore: add "cra-template-redux" as counter example

---
+++ examples/counter/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+
+export default configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
