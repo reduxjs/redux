c37ec8d307192e91349072ae65cdfa7da99ff6da
Jacob Ley <jacob.ley@showcasejobs.com>
2021-03-07 18:09:26 -0800

    fix: Declare "EmptyObject" interface to wrap $CombinedState

--- index.d.ts
+++ index.d.ts
@@ -53,16 +53,17 @@ declare const $CombinedState: unique symbol
  * typed as always undefined, so its never expected to have a meaningful
  * value anyway. It just makes this type distinquishable from plain `{}`.
  */
-export type CombinedState<S> = { readonly [$CombinedState]?: undefined } & S
+interface EmptyObject {
+  readonly [$CombinedState]?: undefined
+}
+export type CombinedState<S> = EmptyObject & S
 
 /**
  * Recursively makes combined state objects partial. Only combined state _root
  * objects_ (i.e. the generated higher level object with keys mapping to
  * individual reducers) are partial.
  */
-export type PreloadedState<S> = Required<S> extends {
-  [$CombinedState]: undefined
-}
+export type PreloadedState<S> = Required<S> extends EmptyObject
   ? S extends CombinedState<infer S1>
     ? {
         [K in keyof S1]?: S1[K] extends object ? PreloadedState<S1[K]> : S1[K]
