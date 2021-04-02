a0794490f75da7b4088c8dc00bf5e86eae801f09
Mark Erikson <mark@isquaredsoftware.com>
2021-04-02 16:20:39 -0400

    Fix initial build import dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import nodeResolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
-import replace from 'rollup-plugin-replace'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+import replace from '@rollup/plugin-replace'
 import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
