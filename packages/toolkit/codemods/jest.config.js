83e18128ada2dbe5b61f23076b7ac8d8ff30f6ac
martinerko <martinerko@gmail.com>
2022-05-04 01:44:40 +1000

    Implement codemods for createReducer and createSlice builder

---
+++ packages/toolkit/codemods/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  automock: false,
+  roots: ['v2.0/__tests__'],
+  transform: { '\\.ts$': ['ts-jest'] },
+}
---
+++ packages/toolkit/codemods/v2.0/__testfixtures__/create-slice-builder/basic.input.ts
@@ -0,0 +1,5 @@
+createSlice({
+  extraReducers: {
+    [todoAdded]: (state, action) => {}
+  }
+})
