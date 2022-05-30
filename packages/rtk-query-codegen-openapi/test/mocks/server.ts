8a1a061814744a59cf6b7b9f1ff781f82d48df9c
Mark Erikson <mark@isquaredsoftware.com>
2022-05-30 15:26:52 -0400

    Fix MSW setup usages

--- packages/rtk-query-codegen-openapi/test/mocks/server.ts
+++ packages/rtk-query-codegen-openapi/test/mocks/server.ts
@@ -7,12 +7,8 @@ import petstoreYAML from '../fixtures/petstore.yaml.mock';
 // This configures a request mocking server with the given request handlers.
 
 export const server = setupServer(
-  rest.get('https://example.com/echo', (req, res, ctx) =>
-    res(ctx.json({ ...req, headers: req.headers.getAllHeaders() }))
-  ),
-  rest.post('https://example.com/echo', (req, res, ctx) =>
-    res(ctx.json({ ...req, headers: req.headers.getAllHeaders() }))
-  ),
+  rest.get('https://example.com/echo', (req, res, ctx) => res(ctx.json({ ...req, headers: req.headers.all() }))),
+  rest.post('https://example.com/echo', (req, res, ctx) => res(ctx.json({ ...req, headers: req.headers.all() }))),
 
   rest.get('https://petstore3.swagger.io/api/v3/openapi.json', (req, res, ctx) => res(ctx.json(petstoreJSON))),
   rest.get('https://petstore3.swagger.io/api/v3/openapi.yaml', (req, res, ctx) =>
