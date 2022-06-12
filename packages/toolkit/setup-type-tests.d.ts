cd525e223dab69bd7db9aacdc3b3c4dd3b04d66e
FaberVitale <faber.vitale@gmail.com>
2022-06-12 10:47:55 +0200

    fix: type-tests error in TS >= 4.4

--- packages/toolkit/setup-type-tests.d.ts
+++ packages/toolkit/setup-type-tests.d.ts
@@ -1,5 +1,24 @@
-// Not available on old TS versions but used by `@testing-library/user-event`
+// Patches types not available on old TS versions but is used by `@testing-library/user-event`.
 // @see https://stackoverflow.com/a/61505395
-declare class ClipboardItem {
-  constructor(data: { [mimeType: string]: Blob })
+declare module 'lib.dom.ts' {
+  type ClipboardItemData = Promise<ClipboardItemDataType>;
+  type ClipboardItemDataType = string | Blob;
+  type PresentationStyle = "attachment" | "inline" | "unspecified";
+
+  interface ClipboardItemOptions {
+    presentationStyle?: PresentationStyle;
+  }
+
+  interface ClipboardItem {
+    readonly types: ReadonlyArray<string>
+    getType(type: string): Promise<Blob>
+  }
+
+  var ClipboardItem: {
+    prototype: ClipboardItem
+    new (
+      items: Record<string, ClipboardItemData>,
+      options?: ClipboardItemOptions
+    ): ClipboardItem
+  }
 }
