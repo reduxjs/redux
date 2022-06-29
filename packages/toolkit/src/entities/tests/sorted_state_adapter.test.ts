f500dfa4fb6c1d3cbfd294ffcc45cd542100af97
Mark Erikson <mark@isquaredsoftware.com>
2022-06-28 21:36:57 -0400

    Rewrite `updateMany` to ensure stable sorting order

--- packages/toolkit/src/entities/tests/sorted_state_adapter.test.ts
+++ packages/toolkit/src/entities/tests/sorted_state_adapter.test.ts
@@ -343,6 +343,34 @@ describe('Sorted State Adapter', () => {
     })
   })
 
+  it('should maintain a stable sorting order when updating items', () => {
+    interface OrderedEntity {
+      id: string
+      order: number
+      ts: number
+    }
+    const sortedItemsAdapter = createEntityAdapter<OrderedEntity>({
+      sortComparer: (a, b) => a.order - b.order,
+    })
+    const withInitialItems = sortedItemsAdapter.setAll(
+      sortedItemsAdapter.getInitialState(),
+      [
+        { id: 'A', order: 1, ts: 0 },
+        { id: 'B', order: 2, ts: 0 },
+        { id: 'C', order: 3, ts: 0 },
+        { id: 'D', order: 3, ts: 0 },
+        { id: 'E', order: 3, ts: 0 },
+      ]
+    )
+
+    const updated = sortedItemsAdapter.updateOne(withInitialItems, {
+      id: 'C',
+      changes: { ts: 5 },
+    })
+
+    expect(updated.ids).toEqual(['A', 'B', 'C', 'D', 'E'])
+  })
+
   it('should let you update many entities by id in the state', () => {
     const firstChange = { title: 'Zack' }
     const secondChange = { title: 'Aaron' }
