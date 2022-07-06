7c7f1595ecda76dbf4e13be13ac0929cdffb893c
Mark Erikson <mark@isquaredsoftware.com>
2022-07-06 15:28:12 -0400

    Replace legacy counter example with latest CRA+JS template

--- examples/counter/src/setupTests.js
+++ examples/counter/src/setupTests.js
@@ -1,4 +1,5 @@
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-configure({ adapter: new Adapter() })
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect';
