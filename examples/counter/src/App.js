22139ec7ff42c295c98c99ce59b39edc27d37030
Mark Erikson <mark@isquaredsoftware.com>
2022-07-07 13:01:40 -0400

    Change logos to work with CSB

--- examples/counter/src/App.js
+++ examples/counter/src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react'
+import { Counter } from './features/counter/Counter'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={`${process.env.PUBLIC_URL ?? ''}/logo.svg`}
+          className="App-logo"
+          alt="logo"
+        />
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -52,7 +55,7 @@ function App() {
         </span>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
