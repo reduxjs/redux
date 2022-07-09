6dfaca7f69eabddc0d289bfb82c468698340c08f
kahirokunn <okinakahiro@gmail.com>
2022-07-10 08:28:03 +0900

    style: Unified the style of retryCondition comments

--- packages/toolkit/src/query/retry.ts
+++ packages/toolkit/src/query/retry.ts
@@ -34,8 +34,8 @@ export interface RetryOptions {
    */
   backoff?: (attempt: number, maxRetries: number) => Promise<void>
   /**
-   Callback to determine if a retry should be attempted. 
-   Return `true` for another retry and `false` to quit trying prematurely.
+   * Callback to determine if a retry should be attempted.
+   * Return `true` for another retry and `false` to quit trying prematurely.
    */
   shouldRetry?: (error: FetchBaseQueryError, args: BaseQueryArg<BaseQueryFn>, extraArgs: {
     attempt: number
