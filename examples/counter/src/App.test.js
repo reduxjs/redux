06a2b944f58ba2a0bc131ff2f2f22e5b38450660
Enrique Jorge <109369662+enriquejr99@users.noreply.github.com>
2022-07-24 02:41:37 +0100

    Fix lint error (#4391)

--- examples/counter/src/App.test.js
+++ examples/counter/src/App.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(
+  render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(screen.getByText(/learn/i)).toBeInTheDocument();
 });
