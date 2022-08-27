c3b276025bde4c01e47fecef5e18e741faac777c
Mark Erikson <mark@isquaredsoftware.com>
2022-08-27 14:30:32 -0400

    Consolidate RTKQ middleware to simplify stack size
    Previously, the RTKQ middleware was made up of 7 "sub-middleware", each
    encapsulating a different responsibility (polling, caching, etc).
    
    However, that meant that each middleware was called on _every_ action,
    even if it wasn't RTKQ-related. That adds to call stack size.
    
    Almost all the logic runs _after_ the action is handled by the reducers.
    So, I've reworked the files to create handlers (conceptually similar to
    the `(action) => {}` part of a middleware, with a couple extra args),
    and rewritten the top-level middleware to run those in a loop.

--- packages/toolkit/src/query/core/buildMiddleware/devMiddleware.ts
+++ packages/toolkit/src/query/core/buildMiddleware/devMiddleware.ts
@@ -1,47 +1,34 @@
-import type { SubMiddlewareBuilder } from './types'
+import type { InternalHandlerBuilder } from './types'
 
-export const build: SubMiddlewareBuilder = ({
+export const buildDevCheckHandler: InternalHandlerBuilder = ({
   api,
   context: { apiUid },
   reducerPath,
 }) => {
-  return (mwApi) => {
-    let initialized = false
-    return (next) => (action) => {
-      if (!initialized) {
-        initialized = true
-        // dispatch before any other action
-        mwApi.dispatch(api.internalActions.middlewareRegistered(apiUid))
-      }
+  return (action, mwApi) => {
+    if (api.util.resetApiState.match(action)) {
+      // dispatch after api reset
+      mwApi.dispatch(api.internalActions.middlewareRegistered(apiUid))
+    }
 
-      const result = next(action)
-
-      if (api.util.resetApiState.match(action)) {
-        // dispatch after api reset
-        mwApi.dispatch(api.internalActions.middlewareRegistered(apiUid))
-      }
-
+    if (
+      typeof process !== 'undefined' &&
+      process.env.NODE_ENV === 'development'
+    ) {
       if (
-        typeof process !== 'undefined' &&
-        process.env.NODE_ENV === 'development'
+        api.internalActions.middlewareRegistered.match(action) &&
+        action.payload === apiUid &&
+        mwApi.getState()[reducerPath]?.config?.middlewareRegistered ===
+          'conflict'
       ) {
-        if (
-          api.internalActions.middlewareRegistered.match(action) &&
-          action.payload === apiUid &&
-          mwApi.getState()[reducerPath]?.config?.middlewareRegistered ===
-            'conflict'
-        ) {
-          console.warn(`There is a mismatch between slice and middleware for the reducerPath "${reducerPath}".
+        console.warn(`There is a mismatch between slice and middleware for the reducerPath "${reducerPath}".
 You can only have one api per reducer path, this will lead to crashes in various situations!${
-            reducerPath === 'api'
-              ? `
+          reducerPath === 'api'
+            ? `
 If you have multiple apis, you *have* to specify the reducerPath option when using createApi!`
-              : ''
-          }`)
-        }
+            : ''
+        }`)
       }
-
-      return result
     }
   }
 }
