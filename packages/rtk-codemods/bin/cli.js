497e5dfac3dbe260afd6ce857092545c240d12f7
Mark Erikson <mark@isquaredsoftware.com>
2022-10-12 22:17:04 -0400

    Get codemod running with ts-node on deploy

--- packages/rtk-codemods/bin/cli.js
+++ packages/rtk-codemods/bin/cli.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
+const path = require('path');
 
-require('ts-node').register();
+require('ts-node').register({
+  project: path.join(__dirname, './tsconfig.json'),
+});
 
 require('codemod-cli').runTransform(
   __dirname,
