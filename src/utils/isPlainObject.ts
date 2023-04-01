80c44bec58775a2039b51937f65defdeb4fa7203
Luchankin Konstantin <lioeaet@gmail.com>
2023-04-01 15:08:34 +0500

    fix Object.create(null), add tests

--- src/utils/isPlainObject.ts
+++ src/utils/isPlainObject.ts
@@ -3,8 +3,8 @@
  * @returns True if the argument appears to be a plain object.
  */
 export default function isPlainObject(obj: any): boolean {
-  if (typeof obj !== 'object' || obj === null) return false
+  if (!obj) return false
 
-  const proto = Object.getPrototypeOf(obj)
-  return Boolean(proto && !Object.getPrototypeOf(proto))
+  const proto = obj.__proto__
+  return !proto || !Object.getPrototypeOf(proto)
 }
