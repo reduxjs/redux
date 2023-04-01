7981ccf270fb7b4aa5a5532c8e7feaea66ffca7d
Luchankin Konstantin <lioeaet@gmail.com>
2023-04-01 11:19:10 +0500

    isPlainObject optimize

--- src/utils/isPlainObject.ts
+++ src/utils/isPlainObject.ts
@@ -5,10 +5,6 @@
 export default function isPlainObject(obj: any): boolean {
   if (typeof obj !== 'object' || obj === null) return false
 
-  let proto = obj
-  while (Object.getPrototypeOf(proto) !== null) {
-    proto = Object.getPrototypeOf(proto)
-  }
-
-  return Object.getPrototypeOf(obj) === proto
+  const proto = Object.getPrototypeOf(obj)
+  return Boolean(proto && !Object.getPrototypeOf(proto))
 }
