7e6b201de4e1f372043a41a9b60bdbb69fbce877
Bilal Ahmad Khan <49161285+bilalkhan13@users.noreply.github.com>
2023-07-25 16:35:12 +0500

    Optimized the isPlainObject.ts
    In this version, unnecessary iteration through the prototype chain has been eliminated by directly comparing the object's prototype with the base Object.prototype. The code is more concise, easier to read, and performs the same functionality

--- src/utils/isPlainObject.ts
+++ src/utils/isPlainObject.ts
@@ -3,12 +3,6 @@
  * @returns True if the argument appears to be a plain object.
  */
 export default function isPlainObject(obj: any): boolean {
-  if (typeof obj !== 'object' || obj === null) return false
-
-  let proto = obj
-  while (Object.getPrototypeOf(proto) !== null) {
-    proto = Object.getPrototypeOf(proto)
-  }
-
-  return Object.getPrototypeOf(obj) === proto
+  return typeof obj === 'object' && obj !== null && Object.getPrototypeOf(obj) === Object.prototype;
 }
+
