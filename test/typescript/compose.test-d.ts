8169d6f25ed7b3147fed950e23152ff7992270e4
Arya Emami <aryaemami59@yahoo.com>
2024-03-09 05:39:29 -0600

    Migrate type tests inside `compose.test-d.ts` to Vitest

--- test/typescript/compose.test-d.ts
+++ test/typescript/compose.test-d.ts
@@ -1,52 +1,70 @@
 import { compose } from 'redux'
 
-// adapted from DefinitelyTyped/compose-function
+describe('type tests', () => {
+  // adapted from DefinitelyTyped/compose-function
 
-const numberToNumber = (a: number): number => a + 2
-const numberToString = (a: number): string => 'foo'
-const stringToNumber = (a: string): number => 5
+  const numberToNumber = (a: number): number => a + 2
 
-const t1: number = compose(numberToNumber, numberToNumber)(5)
-const t2: string = compose(numberToString, numberToNumber)(5)
-const t3: string = compose(numberToString, stringToNumber)('f')
-const t4: (a: string) => number = compose(
-  (f: (a: string) => number) => (p: string) => 5,
-  (f: (a: number) => string) => (p: string) => 4
-)(numberToString)
+  const numberToString = (a: number): string => 'foo'
 
-const t5: number = compose(stringToNumber, numberToString, numberToNumber)(5)
-const t6: string = compose(
-  numberToString,
-  stringToNumber,
-  numberToString,
-  numberToNumber
-)(5)
+  const stringToNumber = (a: string): number => 5
 
-// rest signature
-const t7: string = compose<string>(
-  numberToString,
-  numberToNumber,
-  stringToNumber,
-  numberToString,
-  stringToNumber
-)('fo')
+  test('compose', () => {
+    expectTypeOf(compose(numberToNumber, numberToNumber)(5)).toBeNumber()
 
-const multiArgFn = (a: string, b: number, c: boolean): string => 'foo'
+    expectTypeOf(compose(numberToString, numberToNumber)(5)).toBeString()
 
-const t8: string = compose(multiArgFn)('bar', 42, true)
-const t9: number = compose(stringToNumber, multiArgFn)('bar', 42, true)
-const t10: string = compose(numberToString, stringToNumber, multiArgFn)(
-  'bar',
-  42,
-  true
-)
+    expectTypeOf(compose(numberToString, stringToNumber)('f')).toBeString()
 
-const t11: number = compose(
-  stringToNumber,
-  numberToString,
-  stringToNumber,
-  multiArgFn
-)('bar', 42, true)
+    expectTypeOf(
+      compose(
+        (f: (a: string) => number) => (p: string) => 5,
+        (f: (a: number) => string) => (p: string) => 4
+      )(numberToString)
+    ).toMatchTypeOf<(a: string) => number>()
 
-const funcs = [stringToNumber, numberToString, stringToNumber]
-const t12 = compose(...funcs)('bar')
+    expectTypeOf(
+      compose(stringToNumber, numberToString, numberToNumber)(5)
+    ).toBeNumber()
+
+    expectTypeOf(
+      compose(numberToString, stringToNumber, numberToString, numberToNumber)(5)
+    ).toBeString()
+
+    // rest signature
+    expectTypeOf(
+      compose<string>(
+        numberToString,
+        numberToNumber,
+        stringToNumber,
+        numberToString,
+        stringToNumber
+      )('fo')
+    ).toBeString()
+
+    const multiArgFn = (a: string, b: number, c: boolean): string => 'foo'
+
+    expectTypeOf(compose(multiArgFn)('bar', 42, true)).toBeString()
+
+    expectTypeOf(
+      compose(stringToNumber, multiArgFn)('bar', 42, true)
+    ).toBeNumber()
+
+    expectTypeOf(
+      compose(numberToString, stringToNumber, multiArgFn)('bar', 42, true)
+    ).toBeString()
+
+    expectTypeOf(
+      compose(
+        stringToNumber,
+        numberToString,
+        stringToNumber,
+        multiArgFn
+      )('bar', 42, true)
+    ).toBeNumber()
+
+    const funcs = [stringToNumber, numberToString, stringToNumber]
+
+    expectTypeOf(compose(...funcs)).toBeCallableWith('bar')
+  })
+})
