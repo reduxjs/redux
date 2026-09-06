91d405a7432d38c729017e33a56495f1453a1e45
Mark Erikson <mark@isquaredsoftware.com>
2026-09-06 17:10:01 -0400

    oxlint: update source for the new lint rules

--- src/utils/warning.ts
+++ src/utils/warning.ts
@@ -4,15 +4,15 @@
  * @param message The warning message.
  */
 export default function warning(message: string): void {
-  /* eslint-disable no-console */
+  /* oxlint-disable no-console */
   if (typeof console !== 'undefined' && typeof console.error === 'function') {
     console.error(message)
   }
-  /* eslint-enable no-console */
+  /* oxlint-enable no-console */
   try {
     // This error was thrown as a convenience so that if you enable
     // "break on all exceptions" in your console,
     // it would pause the execution at this line.
     throw new Error(message)
-  } catch (e) {} // eslint-disable-line no-empty
+  } catch {} // oxlint-disable-line no-empty
 }
